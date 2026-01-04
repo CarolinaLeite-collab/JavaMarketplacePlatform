f9ffb6646514189ea9bf2ac2cbbf5070281db62b
mcosta <1251998@isep.ipp.pt>
2026-01-04 10:02:52 +0000

    resolves #24: added Author class and unit tests

---
+++ src/test/java/TOPSECRET/domain/AuthorTest.java
@@ -0,0 +1,36 @@
+package TOPSECRET.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class AuthorTest {
+    @Test
+    void validNameAuthor() {
+        Author a = new Author("Eça de Queirós");
+        assertEquals("Eça de Queirós", a.getName());
+    }
+
+    @Test
+    void authorNameIsTrimmed() {
+        Author a1 = new Author(" Eça de Queirós ");
+        assertEquals("Eça de Queirós", a1.getName());
+    }
+
+    @Test
+    void capitalizationNameTest() {
+        Author a2 = new Author("Eça De Queirós");
+        Author a3 = new Author("EÇA DE QUEIRÓS");
+        Author a4 = new Author("eça de queirós");
+
+        assertEquals(a2.getLowerCaseName(), a3.getLowerCaseName(), a4.getLowerCaseName());
+    }
+
+    @Test
+    void emptyNameAuthor() { assertThrows(IllegalArgumentException.class, () -> {new Author("   ");}); }
+
+    @Test
+    void nullNameAuthor() { assertThrows(IllegalArgumentException.class, () -> {new Author(null);}); }
+
+}
