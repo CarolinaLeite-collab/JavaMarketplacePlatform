966949a74ce991b770af7cac47b283819a5c6ae4
gui1251991 <1251991@isep.ipp.pt>
2026-01-06 16:50:02 +0000

    Resolves #23: Updated PaymentProcessing class and test

--- src/main/java/TOPSECRET/domain/PaymentProcessing.java
+++ src/main/java/TOPSECRET/domain/PaymentProcessing.java
@@ -5,17 +5,16 @@ public class PaymentProcessing {
     private Price _finalPrice;
     private User _buyer;
     private User _seller;
-    private PaymentMethod _paymentMethod; //to be updated
+    //private PaymentMethod _paymentMethod; //to be updated later
 
-    public PaymentProcessing(Price _finalPrice, User _buyer, User _seller, PaymentMethod _paymentMethod) {
-        if (_finalPrice == null || _buyer == null || _seller == null || _paymentMethod == null) {
+    public PaymentProcessing(Price _finalPrice, User _buyer, User _seller) {
+        if (_finalPrice == null || _buyer == null || _seller == null) {
             throw new IllegalArgumentException("Arguments cannot be null.");
         }
 
         this._finalPrice = _finalPrice;
         this._buyer = _buyer;
         this._seller = _seller;
-        this._paymentMethod = _paymentMethod;
     }
 
     public boolean isSuccessful() {
--- src/test/java/TOPSECRET/domain/PaymentProcessingTest.java
+++ src/test/java/TOPSECRET/domain/PaymentProcessingTest.java
@@ -3,73 +3,92 @@ package TOPSECRET.domain;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 class PaymentProcessingTest {
 
-        @Test
-        void shouldCreatePaymentProcessing_whenAllArgumentsAreValid() {
-            // Arrange
-            Price price = new Price();
-            User buyer = new User();
-            User seller = new User();
-            PaymentProcessing.PaymentMethod method = PaymentProcessing.PaymentMethod.TRANSFER;
+    @Test
+    void shouldCreatePaymentProcessing_whenAllArgumentsAreValid() {
+        // Arrange
+        Price price = new Price(5,Currency.EUR);
+        User buyer = new User(
+                new Name("Ana"),
+                new Address("Rua Vasco da Gama", "123", Address.BuildingType.HOUSE, "Lisboa",
+                        "Lisboa", Address.Country.PORTUGAL, "1000-205", null),
+                new Email("user@hotmail.com"),
+                new Phone (new PhonePrefix("+351"), "930914359")
+        );
+        User seller = new User(
+                new Name("Joao"),
+                new Address("Rua Vasco da Gama", "123", Address.BuildingType.HOUSE, "Lisboa",
+                        "Lisboa", Address.Country.PORTUGAL, "1000-205", null),
+                new Email("user@hotmail.com"),
+                new Phone (new PhonePrefix("+351"), "930914359")
+        );
 
-            // Act
-            PaymentProcessing pp = new PaymentProcessing(price, buyer, seller, method);
+        // Act
+        PaymentProcessing pp = new PaymentProcessing(price, buyer, seller);
 
-            // Assert
-            assertNotNull(pp);
-        }
-
-        @Test
-        void shouldThrowException_whenPriceIsNull() {
-            // Arrange
-            User buyer = new User();
-            User seller = new User();
-            PaymentProcessing.PaymentMethod method = PaymentProcessing.PaymentMethod.TRANSFER;
-
-            // Act + Assert
-            assertThrows(IllegalArgumentException.class, () -> {
-                new PaymentProcessing(null, buyer, seller, method);
-            });
-        }
-
-        @Test
-        void shouldThrowException_whenBuyerIsNull() {
-            // Arrange
-            Price price = new Price();
-            User seller = new User();
-            PaymentProcessing.PaymentMethod method = PaymentProcessing.PaymentMethod.TRANSFER;
-
-            // Act + Assert
-            assertThrows(IllegalArgumentException.class, () -> {
-                new PaymentProcessing(price, null, seller, method);
-            });
-        }
-
-        @Test
-        void shouldThrowException_whenSellerIsNull() {
-            // Arrange
-            Price price = new Price();
-            User buyer = new User();
-            PaymentProcessing.PaymentMethod method = PaymentProcessing.PaymentMethod.TRANSFER;
-
-            // Act + Assert
-            assertThrows(IllegalArgumentException.class, () -> {
-                new PaymentProcessing(price, buyer, null, method);
-            });
-        }
-
-        @Test
-        void shouldThrowException_whenPaymentMethodIsNull() {
-            // Arrange
-            Price price = new Price();
-            User buyer = new User();
-            User seller = new User();
-
-            // Act + Assert
-            assertThrows(IllegalArgumentException.class, () -> {
-                new PaymentProcessing(price, buyer, seller, null);
-            });
-        }
+        // Assert
+        assertNotNull(pp);
     }
+
+    @Test
+    void shouldThrowException_whenPriceIsNull() {
+        // Arrange
+        User buyer = new User(
+                new Name("Ana"),
+                new Address("Rua Vasco da Gama", "123", Address.BuildingType.HOUSE, "Lisboa",
+                        "Lisboa", Address.Country.PORTUGAL, "1000-205", null),
+                new Email("user@hotmail.com"),
+                new Phone (new PhonePrefix("+351"), "930914359")
+        );
+        User seller = new User(
+                new Name("Joao"),
+                new Address("Rua Vasco da Gama", "123", Address.BuildingType.HOUSE, "Lisboa",
+                        "Lisboa", Address.Country.PORTUGAL, "1000-205", null),
+                new Email("user@hotmail.com"),
+                new Phone (new PhonePrefix("+351"), "930914359")
+        );
+
+        // Act + Assert
+        assertThrows(IllegalArgumentException.class, () -> {
+            new PaymentProcessing(null, buyer, seller);
+        });
+    }
+
+    @Test
+    void shouldThrowException_whenBuyerIsNull() {
+        // Arrange
+        Price price = new Price(5,Currency.EUR);
+        User seller = new User(
+                new Name("Joao"),
+                new Address("Rua Vasco da Gama", "123", Address.BuildingType.HOUSE, "Lisboa",
+                        "Lisboa", Address.Country.PORTUGAL, "1000-205", null),
+                new Email("user@hotmail.com"),
+                new Phone (new PhonePrefix("+351"), "930914359")
+        );
+
+        // Act + Assert
+        assertThrows(IllegalArgumentException.class, () -> {
+            new PaymentProcessing(price, null, seller);
+        });
+    }
+
+    @Test
+    void shouldThrowException_whenSellerIsNull() {
+        // Arrange
+        Price price = new Price(5,Currency.EUR);
+        User buyer = new User(
+                new Name("Ana"),
+                new Address("Rua Vasco da Gama", "123", Address.BuildingType.HOUSE, "Lisboa",
+                        "Lisboa", Address.Country.PORTUGAL, "1000-205", null),
+                new Email("user@hotmail.com"),
+                new Phone (new PhonePrefix("+351"), "930914359")
+        );
+        // Act + Assert
+        assertThrows(IllegalArgumentException.class, () -> {
+            new PaymentProcessing(price, buyer, null);
+        });
+    }
+
 }
