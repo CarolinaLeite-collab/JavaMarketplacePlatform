57f36d1cc8a1bb2b6e0aa70e288cde61cabf9527
TiagoLeite1252008 <1252008@isep.ipp.pt>
2026-01-06 18:08:12 +0000

    resolves #33 add PublicatioInfo class and tests

---
+++ src/test/java/TOPSECRET/domain/PublicationInfoTest.java
@@ -0,0 +1,31 @@
+package TOPSECRET.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PublicationInfoTest {
+
+    @Test
+    void constructorWhithValidArgumentsPublicationInfo() {
+        PublicationInfo publicationInfo = new PublicationInfo(
+                new Title ("title"),
+                Genre.ACTION,
+                new Author ("Eça de Queirós"),
+                new Edition(
+                    new ISSN("1018-4783"),
+                    new ISBN(9789720048758L),
+                    30,
+                    3,
+                    LocalDate.of(2001, 4, 23),
+                    Binding.SADDLE_STITCH,
+                    new Description("Amazing Magazine"),
+                    new Dimension(21, 29.7, 1, DimensionUnit.CENTIMETERS),
+                    new Weight(224.7, Weight.WeightUnit.GRAMS),
+                    Language.of("pt", "Portuguese", "Português")),
+                new Publisher("My Publisher")
+        );
+    }
+}
