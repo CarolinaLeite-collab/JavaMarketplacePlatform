a59acfc469a1adcc2a3d7531f41f25864aceaf2e
Carolina Leite <1251987@isep.ipp.pt>
2026-01-14 22:33:47 +0000

    Add PublicationRepo class and publicationAlreadyExists method (refs #78)

--- src/main/java/TOPSECRET/domain/PublicationRepo.java
+++ src/main/java/TOPSECRET/domain/PublicationRepo.java
@@ -1,4 +1,33 @@
 package TOPSECRET.domain;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PublicationRepo {
+    private List<Publication> _publications;
+
+    public PublicationRepo() {
+        _publications = new ArrayList<>();
+    }
+
+    /*public Publication create(PublicationType publicationType, String id, Title title, PublicationDate publicationDate, ...){
+
+        // instantiate a new myPublicationInfo
+        Publication myPublication = new Publication ();
+
+        //already present?
+
+        //add myPublicationInfo to repo
+        _publications.add(myPublication);
+
+        //return myPublication
+        return myPublication;
+    }*/
+
+    private boolean publicationAlreadyExists (Publication myPublication) {
+        for (Publication publication : _publications) {
+            if (publication.equals(myPublication))
+                return true;
+        }
+        return false;
+    }
 }
