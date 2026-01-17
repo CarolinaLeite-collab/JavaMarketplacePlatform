99dcdbd7a6e059909e133fc25209b71cfbc327e1
Jose Sousa <1251994@isep.ipp.pt>
2026-01-17 19:26:13 +0000

    Resolves #158: Add new AuthorRepo class and AuthorRepoTests

---
+++ src/main/java/TOPSECRET/domain/AuthorRepo.java
@@ -0,0 +1,33 @@
+package TOPSECRET.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AuthorRepo {
+
+    private List<Author> authors = new ArrayList<>();
+
+    // Repository creates and returns the Author
+    public Author create(String authorName) {
+        if (authorName == null || authorName.isBlank()) {
+            throw new IllegalArgumentException("Author name is mandatory");
+        }
+
+        String normalizedName = authorName.trim();
+        if (existsByName(normalizedName)){
+            throw new IllegalStateException("Author already exists");
+        }
+
+            Author author = new Author(normalizedName);
+            authors.add(author);
+            return author;
+    }
+
+    public boolean existsByName(String name) {
+        return authors.stream().anyMatch(a -> a.getName().equalsIgnoreCase(name));
+    }
+
+    public List<Author> findAll() {
+        return new ArrayList<>(authors);
+    }
+}
