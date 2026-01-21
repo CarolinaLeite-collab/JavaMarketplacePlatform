447c3917d9a34ce61bbe48e0866f75bdf9f5bc17
Vitor Barros <1252009@isep.ipp.pt>
2026-01-21 12:29:57 +0000

    #189 - added Javadoc for class

--- src/main/java/TOPSECRET/controller/GetListOfOfficialGenresController.java
+++ src/main/java/TOPSECRET/controller/GetListOfOfficialGenresController.java
@@ -7,6 +7,15 @@ import TOPSECRET.domain.User;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Controller responsible for retrieving the list of official genres.
+ * <p>
+ * This controller acts as an intermediary between the user interface
+ * and the domain layer, delegating the retrieval of official genres
+ * to the {@link GenreRepo}.
+ * </p>
+ */
+
 public class GetListOfOfficialGenresController {
 
     private GenreRepo _gr;
