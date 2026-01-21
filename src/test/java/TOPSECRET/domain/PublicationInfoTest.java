2b6e07cc61d098d0b091acf0645c244f6898a4b8
filipamesquita <1251990@isep.ipp.pt>
2026-01-21 12:57:24 +0000

    #33 Fix PublicationInfo tests

--- src/test/java/TOPSECRET/domain/PublicationInfoTest.java
+++ src/test/java/TOPSECRET/domain/PublicationInfoTest.java
@@ -9,27 +9,31 @@ import static org.junit.jupiter.api.Assertions.*;
 class PublicationInfoTest {
 
     @Test
-    void constructorWhithValidArgumentsPublicationInfo() {
+    void shouldCreatePublicationInfoForBook() {
         PublicationInfo publicationInfoBook = new PublicationInfo(
-                new Title ("title"),
-                Genre.ACTION,
-                new Author ("Eça de Queirós"),
+                new Title("title"),
+                new Genre("action"),
+                new Author("Eça de Queirós"),
                 new Edition(
-                    new ISBN("9780306406157"),
-                    30,
-                    3,
-                    LocalDate.of(2001, 4, 23),
-                    Binding.SADDLE_STITCH,
-                    new Description("Amazing Magazine"),
-                    new Dimension(21, 29.7, 1, DimensionUnit.CENTIMETERS),
-                    new Weight(224.7, Weight.WeightUnit.GRAMS),
-                    Language.of("pt", "Portuguese", "Português")),
+                        new ISBN("9780306406157"),
+                        30,
+                        3,
+                        LocalDate.of(2001, 4, 23),
+                        Binding.SADDLE_STITCH,
+                        new Description("Amazing Magazine"),
+                        new Dimension(21, 29.7, 1, DimensionUnit.CENTIMETERS),
+                        new Weight(224.7, Weight.WeightUnit.GRAMS),
+                        Language.of("pt", "Portuguese", "Português")),
                 new Publisher("My Publisher")
         );
+        assertNotNull(publicationInfoBook);
+    }
+    @Test
+    void shouldCreatePublicationInfoForMagazine() {
 
         PublicationInfo publicationInfoMagazine = new PublicationInfo(
                 new Title ("title"),
-                Genre.ACTION,
+                new Genre("action"),
                 new Author ("Eça de Queirós"),
                 new Edition(
                         new ISSN("1018-4783"),
@@ -43,5 +47,6 @@ class PublicationInfoTest {
                         Language.of("pt", "Portuguese", "Português")),
                 new Publisher("My Publisher")
         );
+        assertNotNull(publicationInfoMagazine);
     }
 }
