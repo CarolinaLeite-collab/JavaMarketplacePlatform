c146475802fca0d2828b24bd31f223d87d1fdf1c
filipamesquita <1251990@isep.ipp.pt>
2026-01-22 11:06:26 +0000

    Improve PIT mutation coverage for ListOfPublicationsInMyLibraryController #117

--- src/test/java/TOPSECRET/controller/ListOfPublicationsInMyLibraryControllerTest.java
+++ src/test/java/TOPSECRET/controller/ListOfPublicationsInMyLibraryControllerTest.java
@@ -3,6 +3,7 @@ import TOPSECRET.domain.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.Year;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,10 +11,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class ListOfPublicationsInMyLibraryControllerTest {
 
     private User _user;
-    private LibraryRepo libraryRepo;
-    private ListOfPublicationsInMyLibraryController controller;
+    private LibraryRepo _libraryRepo;
+    private ListOfPublicationsInMyLibraryController _controller;
 
     @BeforeEach
+
     void setUp() {
 
         _user = new User(
@@ -21,22 +23,47 @@ class ListOfPublicationsInMyLibraryControllerTest {
                 new Email("test@isep.com")
         );
 
-        libraryRepo = new LibraryRepo();
-        controller = new ListOfPublicationsInMyLibraryController(libraryRepo, _user);
+        _libraryRepo = new LibraryRepo();
+        _controller = new ListOfPublicationsInMyLibraryController(_libraryRepo, _user);
     }
 
 
     @Test
     void shouldReturnEmptyList_whenLibraryExistsButEmpty() {
         // Arrange
-        Library mylibrary = libraryRepo.create(_user);
+        Library myLibrary = _libraryRepo.create(_user);
 
         // Act
-        List<PublicationDetails> result = controller.getListOfPublications(_user);
+        List<PublicationDetails> result = _controller.getListOfPublications(_user);
 
         // Assert
         assertNotNull(result);
         assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void shouldReturnListOfPublicationsInLibrary() {
+
+        Library myLibrary= _libraryRepo.create(_user);
+        Publication p = Publication.builder()
+                .type(new PublicationType("BOOK"))
+                .identifier(new ISBN("9780691181950"))
+                .year(Year.of(2019))
+                .title(new Title("How to Keep Your Cool"))
+                .author(new Author("Seneca"))
+                .publisher(new Publisher("Penguin"))
+                .build();
+
+        myLibrary.addPublicationToLibrary(p);
+
+        List<PublicationDetails> result = _controller.getListOfPublications(_user);
+
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        assertEquals(p.getTitle(), result.get(0).getTitle());
+        assertEquals(p.getAuthor(), result.get(0).getAuthor());
+        assertEquals(p.getPublicationType(), result.get(0).getPublicationType());
+        assertEquals(p.getIdentifier(), result.get(0).getIdentifier());
     }
 
 
@@ -44,7 +71,7 @@ class ListOfPublicationsInMyLibraryControllerTest {
     void shouldThrowException_whenLibraryNotFound() {
         IllegalStateException exception = assertThrows(
                 IllegalStateException.class,
-                () -> libraryRepo.findByUser(_user)
+                () -> _libraryRepo.findByUser(_user)
         );
         assertEquals("Library not found for user: Zé Isep", exception.getMessage());
     }
