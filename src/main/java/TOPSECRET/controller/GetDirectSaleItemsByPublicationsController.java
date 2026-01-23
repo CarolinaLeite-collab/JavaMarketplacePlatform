c321700b9be84ca0c654ebe3f21d776b3e461e10
mcosta <1251998@isep.ipp.pt>
2026-01-23 11:26:46 +0000

    Added Javadoc in GetDirectSaleItemsByPublicationsController class #207

--- src/main/java/TOPSECRET/controller/GetDirectSaleItemsByPublicationsController.java
+++ src/main/java/TOPSECRET/controller/GetDirectSaleItemsByPublicationsController.java
@@ -4,6 +4,23 @@ import TOPSECRET.domain.*;
 
 import java.util.List;
 
+/**
+ * Controller responsible for retrieving the list of items that are currently
+ * on direct sale for a given publication.
+ *
+ * <p>
+ * This controller belongs to the application layer and acts as an intermediary
+ * between the user interface and the domain layer. It coordinates the request
+ * to obtain direct sale items without containing business logic.
+ * </p>
+ *
+ * <p>
+ * The controller delegates the retrieval of data to {@link DirectSaleRepo},
+ * ensuring a clear separation of concerns and keeping the controller stateless.
+ * </p>
+ */
+
+
 public class GetDirectSaleItemsByPublicationsController {
     private DirectSaleRepo _dsr;
 
