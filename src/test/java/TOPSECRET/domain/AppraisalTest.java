6bce501a316e052e5f55af0ba804938a585ef318
filipamesquita <1251990@isep.ipp.pt>
2026-02-27 11:02:14 +0000

    Refactor Appraisal tests to isolated unit tests #216

--- src/test/java/TOPSECRET/domain/AppraisalTest.java
+++ src/test/java/TOPSECRET/domain/AppraisalTest.java
@@ -5,88 +5,113 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class AppraisalTest {
 
-    private Price price(int value, Currency currency) {
-        return new Price(value, currency);
-    }
+    private static final LocalDateTime _fixedDate =  LocalDateTime.of(2025, 1, 1, 10, 0);
+    private static final String _validDescription =  "The book is in great condition, with only slight use marks.";
 
     @Test
     void tests_creation_of_valid_appraisal() {
-        Price value = price(10, Currency.EUR);
-        LocalDateTime date = LocalDateTime.now();
-        String description = "Harry Potter and The Philosopher's Stone";
 
-        Appraisal appraisal = new Appraisal(value, date, description);
+        Price _priceDouble = mock(Price.class);
+        Appraisal appraisal = new Appraisal(_priceDouble, _fixedDate,_validDescription);
 
-        assertEquals(value, appraisal.getValueEstimate());
-        assertEquals(date, appraisal.getAppraisalDate());
-        assertEquals(description, appraisal.getObjectDescription());
+        assertEquals(_priceDouble, appraisal.getValueEstimate());
+        assertEquals(_fixedDate, appraisal.getAppraisalDate());
+        assertEquals(_validDescription, appraisal.getObjectDescription());
     }
 
     @Test
     void should_return_exception_for_null_value_estimate() {
-        LocalDateTime now = LocalDateTime.now();
 
         assertThrows(IllegalArgumentException.class,
-                () -> new Appraisal(null, now, "The book is in great condition, with only slight use marks."));
+                () -> new Appraisal(null, _fixedDate, _validDescription));
     }
 
     @Test
     void should_return_exception_for_null_appraisal_date() {
+        Price priceDouble = mock(Price.class);
+
         assertThrows(IllegalArgumentException.class,
-                () -> new Appraisal(price(100, Currency.GBP), null, "The book is in great condition, with only slight use marks."));
+                () -> new Appraisal(priceDouble, null, _validDescription));
     }
 
     @Test
     void should_return_exception_for_empty_description() {
-        LocalDateTime date = LocalDateTime.now();
+
+        Price priceDouble = mock(Price.class);
 
         assertThrows(IllegalArgumentException.class,
-                () -> new Appraisal(price(56, Currency.EUR), date, ""));
+                () -> new Appraisal(priceDouble, _fixedDate, ""));
+    }
+    @Test
+    void should_return_exception_for_blank_description() {
+
+        Price priceDouble = mock(Price.class);
         assertThrows(IllegalArgumentException.class,
-                () -> new Appraisal(price(56, Currency.EUR), date, "   "));
+                () -> new Appraisal(priceDouble, _fixedDate, "   "));
     }
 
     @Test
     void tests_whether_equality_depends_on_all_fields() {
-        Price value = price(5, Currency.AUD);
-        LocalDateTime date = LocalDateTime.of(2026, 1, 3, 13, 40);
-        String description = "The book is in a poor condition, with extensive use marks and some ripped pages.";
 
-        Appraisal a1 = new Appraisal(value, date, description);
-        Appraisal a2 = new Appraisal(value, date, description);
+        Price _priceDouble = mock(Price.class);
+
+        Appraisal a1 = new Appraisal(_priceDouble, _fixedDate,_validDescription);
+        Appraisal a2 = new Appraisal(_priceDouble, _fixedDate,_validDescription);
 
         assertEquals(a1, a2);
         assertEquals(a1.hashCode(), a2.hashCode());
     }
 
     @Test
-    void tests_whether_different_appraisals_are_not_equal() {
-        Price value1 = price(100, Currency.JPY);
-        Price value2 = price(200, Currency.CHF);
-        LocalDateTime date = LocalDateTime.of(2021, 3, 1, 14, 20);
-        String description = "Book";
+    void tests_whether_different_price_makes_appraisal_not_equal() {
+        Price priceDouble = mock(Price.class);
+        Price priceDouble1 = mock(Price.class);
 
-        Appraisal a1 = new Appraisal(value1, date, description);
-        Appraisal a2 = new Appraisal(value2, date, description);
+        Appraisal a1 = new Appraisal(priceDouble, _fixedDate, _validDescription );
+        Appraisal differentPrice = new Appraisal(priceDouble1, _fixedDate, _validDescription );
+
+        assertNotEquals(a1, differentPrice);
+    }
+
+    @Test
+    void tests_whether_different_date_makes_appraisals_not_equal() {
+
+        Price _priceDouble = mock(Price.class);
+        LocalDateTime _differentDate = LocalDateTime.of(2025, 1, 5, 10, 0);
+
+        Appraisal a1 = new Appraisal(_priceDouble, _fixedDate, _validDescription );
+        Appraisal differentDate = new Appraisal(_priceDouble, _differentDate , _validDescription );
+
+        assertNotEquals(a1, differentDate);
+    }
+
+    @Test
+    void tests_whether_different_description_makes_appraisals_not_equal() {
+
+        Price _priceDouble = mock(Price.class);
+
+        Appraisal a1 = new Appraisal(_priceDouble, _fixedDate, _validDescription);
+        Appraisal a2 = new Appraisal(_priceDouble, _fixedDate, "Different description");
 
         assertNotEquals(a1, a2);
     }
 
     @Test
     void tests_whether_to_string_contains_key_information() {
-        Price value = price(104, Currency.EUR);
-        LocalDateTime date = LocalDateTime.of(2023, 9, 27, 12, 30);
-        String description = "The book is in great condition, with no use marks.";
+        Price priceDouble = mock(Price.class);
+        when(priceDouble.toString()).thenReturn("10.0 €");
 
-        Appraisal appraisal = new Appraisal(value, date, description);
+        Appraisal appraisal = new Appraisal(priceDouble, _fixedDate, _validDescription );
         String text = appraisal.toString();
 
         assertTrue(text.contains("Appraisal"));
-        assertTrue(text.contains("The book is in great condition, with no use marks."));
-        assertTrue(text.contains("104.0 €"));
+        assertTrue(text.contains(_validDescription));
+        assertTrue(text.contains("10.0 €"));
     }
 
 }
