13c186fccb49eb05c43f7a8688f6b3b64eeafa0a
Carolina Leite <1251987@isep.ipp.pt>
2026-03-02 18:07:31 +0000

    Adds Tests doubles. Adds to #223

--- src/test/java/TOPSECRET/domain/CountryFactoryTest.java
+++ src/test/java/TOPSECRET/domain/CountryFactoryTest.java
@@ -1,27 +1,45 @@
 package TOPSECRET.domain;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedConstruction;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mockConstruction;
+import static org.mockito.Mockito.when;
 
 class CountryFactoryTest {
 
     @Test
-    void shouldSuccessfullyCreateCountry () throws InstantiationException {
+    void shouldSuccessfullyCreateCountry() throws InstantiationException {
         // arrange
         String countryName = "Deutschland";
-        CountryFactory factory = new CountryFactory();
-        // act
-        Country newCountry = factory.create(countryName);
-        //assert
-        assertEquals(countryName,newCountry.getCountryName());
+        try (MockedConstruction<Country> mocked =
+                     mockConstruction(Country.class,
+                             (mock, context) -> {
+                                 when(mock.getCountryName())
+                                         .thenReturn("Deutschland");
+                             })) {
+            CountryFactory factory = new CountryFactory();
+            // act
+            Country newCountry = factory.create(countryName);
+            //assert
+            assertEquals(countryName, newCountry.getCountryName());
+        }
     }
+
     @Test
-    void shouldThrowExceptionWhenCreateCountry () throws InstantiationException {
+    void shouldThrowExceptionWhenCreateCountry() {
         // arrange
-        String countryName = "";
-        CountryFactory factory = new CountryFactory();
-        // act and assert
-        assertThrows(InstantiationException.class,() -> factory.create(countryName));
+        String countryName = "hgfdfjygfgvjh";
+        try (MockedConstruction<Country> mocked =
+                     mockConstruction(Country.class, (mock, context) -> {
+                         throw new RuntimeException("dfrdfgtfrghfvhbgvbh");
+                     })) {
+            //SUT
+            CountryFactory factory = new CountryFactory();
+
+            // act and assert
+            assertThrows(InstantiationException.class, () -> factory.create(countryName));
+        }
     }
 }
