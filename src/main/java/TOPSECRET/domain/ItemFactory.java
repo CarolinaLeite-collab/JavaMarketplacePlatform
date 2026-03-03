1fa53f17503b5c459e837250bd64d7a5279a1d87
Carolina Leite <1251987@isep.ipp.pt>
2026-03-03 10:45:31 +0000

    Implements a create method in ItemFactory class. Adds to #231

--- src/main/java/TOPSECRET/domain/ItemFactory.java
+++ src/main/java/TOPSECRET/domain/ItemFactory.java
@@ -1,4 +1,12 @@
 package TOPSECRET.domain;
 
 public class ItemFactory {
+    public Item create(Publication publication, Condition condition) throws InstantiationException {
+        try {
+            return new Item(publication, condition);
+        }
+        catch (final Exception e) {
+            throw new InstantiationException("Unable to instantiate Item: " + e.getMessage());
+        }
+    }
 }
