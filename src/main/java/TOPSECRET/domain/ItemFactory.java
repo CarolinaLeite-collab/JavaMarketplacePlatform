8cca0a50fa482842cd231e37fc03424b2c266c3f
Carolina Leite <1251987@isep.ipp.pt>
2026-03-03 13:42:35 +0000

    Adds javadoc description to ItemFactory.Closes #231

--- src/main/java/TOPSECRET/domain/ItemFactory.java
+++ src/main/java/TOPSECRET/domain/ItemFactory.java
@@ -1,4 +1,12 @@
 package TOPSECRET.domain;
+/**
+ * Factory responsible for creating {@link Item} instances.
+ * <p>
+ * This class encapsulates the instantiation logic of {@code Item},
+ * centralizing object creation and isolating clients from constructor
+ * details. Any exception thrown during the creation process is wrapped
+ * into an {@link InstantiationException}.
+ */
 
 public class ItemFactory {
     public Item create(Publication publication, Condition condition) throws InstantiationException {
