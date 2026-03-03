5504939d79c38d46d7281cace52582d2ceb650af
Carolina Leite <1251987@isep.ipp.pt>
2026-03-03 10:44:14 +0000

    Adds Isolated unit tests to ItemFactoryTest class. Adds to #231

--- src/test/java/TOPSECRET/domain/ItemFactoryTest.java
+++ src/test/java/TOPSECRET/domain/ItemFactoryTest.java
@@ -1,7 +1,51 @@
 package TOPSECRET.domain;
 
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedConstruction;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class ItemFactoryTest {
 
+    @Test
+    void shouldSuccessfullyCreateItem() throws InstantiationException {
+        //arrange
+        Publication publication = mock(Publication.class);
+        Condition condition = Condition.LIKE_NEW;
+        try (MockedConstruction<Item> mocked =
+                     mockConstruction(Item.class,
+                             (mock, context) -> {
+                                 when(mock.getCondition())
+                                         .thenReturn(condition);
+                             })) {
+
+        }
+        //SUT
+        ItemFactory factory = new ItemFactory();
+
+        //act
+        Item newItem = factory.create(publication, condition);
+
+        //assert
+        assertEquals(condition, newItem.getCondition());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenCreateItem() throws InstantiationException {
+        //arrange
+        Publication publication = mock(Publication.class);
+        Condition condition = Condition.LIKE_NEW;
+        try (MockedConstruction<Item> mocked =
+                     mockConstruction(Item.class, (mock, context) -> {
+                         throw new RuntimeException("WRONG");
+                     })) {
+
+            //SUT
+            ItemFactory factory = new ItemFactory();
+
+            // act and assert
+            assertThrows(InstantiationException.class, () -> factory.create(publication, condition));
+        }
+    }
 }
