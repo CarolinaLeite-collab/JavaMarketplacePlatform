ac6a25b507586ea228abb3b11b642e08e1f113e4
Carolina Leite <1251987@isep.ipp.pt>
2026-03-06 15:47:06 +0000

    Refactor RegisterCountryController to enforce admin authorization and null validation. Adds to #156

--- src/main/java/TOPSECRET/controller/RegisterCountryController.java
+++ src/main/java/TOPSECRET/controller/RegisterCountryController.java
@@ -2,6 +2,7 @@ package TOPSECRET.controller;
 
 import TOPSECRET.domain.Country;
 import TOPSECRET.domain.CountryRepo;
+import TOPSECRET.domain.Role;
 import TOPSECRET.domain.User;
 
 import java.util.Objects;
@@ -17,13 +18,16 @@ import java.util.Objects;
 public class RegisterCountryController {
     private final CountryRepo _countryRepo;
 
-    public RegisterCountryController(CountryRepo countryRepo, User _admin) {
+    public RegisterCountryController(CountryRepo countryRepo) {
         this._countryRepo = Objects.requireNonNull(countryRepo, "CountryRepo cannot be null");
     }
 
-    public Country registerCountry(String countryName, User admin) {
+    public Country registerCountry(String countryName, User user) {
+        Objects.requireNonNull(user, "User cannot be null");
+
+        if (!user.hasRole(Role.ADMIN)) {
+            throw new SecurityException("User is not authorized to register countries");
+        }
         return _countryRepo.registerCountry(countryName);
-
     }
-
 }
