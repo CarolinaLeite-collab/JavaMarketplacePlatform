5df5b3dccb8fcca3e0149f2272fe768ea310a220
Carolina Leite <1251987@isep.ipp.pt>
2026-03-07 13:34:36 +0000

    Minor cleanup in RegisterCountryControllerTest: remove unused imports. Adds to #156

--- src/test/java/TOPSECRET/controller/RegisterCountryControllerTest.java
+++ src/test/java/TOPSECRET/controller/RegisterCountryControllerTest.java
@@ -5,24 +5,16 @@ import TOPSECRET.domain.Country;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.configuration.IMockitoConfiguration;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class RegisterCountryControllerTest {
-    private CountryFactory _factory;
     private CountryRepo _countryRepo;
     private RegisterCountryController _sut;
-    private User _admin;
 
     @BeforeEach
-    void setUp() throws InstantiationException {
-
+    void setUp(){
         _countryRepo = mock(CountryRepo.class);
         _sut = new RegisterCountryController(_countryRepo);
     }
@@ -38,11 +30,10 @@ class RegisterCountryControllerTest {
         // act and assert
         assertThrows(NullPointerException.class, () ->
                 new RegisterCountryController(null));
-
     }
 
     @Test
-    void shouldRegisterCountrySuccessfully() throws InstantiationException {
+    void shouldRegisterCountrySuccessfully(){
         //Arrange
         User user = mock(User.class);
         when(user.hasRole(Role.ADMIN)).thenReturn(true);
@@ -50,29 +41,21 @@ class RegisterCountryControllerTest {
         when(_countryRepo.registerCountry("Portugal")).thenReturn(portugal);
         //Act
         Country country = _sut.registerCountry("Portugal", user);
-
         // Assert
         assertNotNull(country);
-
-        // asserts a efeitos secundários
-        //assertEquals("PORTUGAL", country.getCountryName());
-        //assertEquals(1, _countryRepo.getAllCountries().size());
-        //assertEquals(country, _countryRepo.getAllCountries().get(0));
     }
 
     @Test
-    void shouldNotRegisterCountrySuccessfullyIfUserNotAdmin() throws SecurityException {
+    void shouldNotRegisterCountrySuccessfullyIfUserNotAdmin(){
         //Arrange
         User user = mock(User.class);
         when(user.hasRole(Role.ADMIN)).thenReturn(false);
-        List<Country> countries = _countryRepo.getAllCountries();
-
         // Act & Assert
         Assertions.assertThrows(SecurityException.class, () -> _sut.registerCountry("Portugal", user));
     }
 
     @Test
-    void shouldNotRegisterNullUser() throws InstantiationException {
+    void shouldNotRegisterNullUser(){
         // Arrange
         // Act & Assert
         assertThrows(NullPointerException.class,
