2ea2d855e83de73680110a84ccac2f029ca4e8b0
rafaelar <1252002@isep.ipp.pt>
2026-03-11 09:30:09 +0000

    Refs #217: Updates tests for GetListOfOfficialGenres, focusing on isolation

--- src/test/java/TOPSECRET/controller/GetListOfOfficialGenresControllerTest.java
+++ src/test/java/TOPSECRET/controller/GetListOfOfficialGenresControllerTest.java
@@ -7,30 +7,19 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class GetListOfOfficialGenresControllerTest {
 
     private User _user;
-    private GenreFactory _genreFactory;
     private GenreRepo _genreRepo;
     private GetListOfOfficialGenresController _getListOfOfficialGenresController;
 
-    @BeforeEach
-    void setUp() {
-
-        _user = new User(
-                new Name("Zé Isep"),
-                new Email("ze@isep.pt")
-        );
-        _genreFactory = new GenreFactory();
-        _genreRepo = new GenreRepo(_genreFactory);
-        _getListOfOfficialGenresController = new GetListOfOfficialGenresController(_genreRepo, _user);
-
-    }
-
     @Test
     void test_a_constructor_get_official_genres_controller() {
-
+        _user = mock(User.class);
+        _genreRepo = mock(GenreRepo.class);
+        _getListOfOfficialGenresController = new GetListOfOfficialGenresController(_genreRepo, _user);
         //act
         new GetListOfOfficialGenresController(_genreRepo, _user);
 
@@ -38,23 +27,33 @@ class GetListOfOfficialGenresControllerTest {
 
     @Test
     void test_get_list_of_official_genres_should_return_list_with_genres() {
-
         //arrange
-        _genreRepo.addGenre("fiction");
-        _genreRepo.addGenre("romance");
+        _user = mock(User.class);
+        _genreRepo = mock(GenreRepo.class);
+        _getListOfOfficialGenresController = new GetListOfOfficialGenresController(_genreRepo, _user);
+
+        Genre genre1 = mock(Genre.class);
+        when(genre1.getGenre()).thenReturn("fiction");
+        Genre genre2 = mock(Genre.class);
+        when(genre2.getGenre()).thenReturn("romance");
+
+        when(_genreRepo.getListOfOfficialGenres()).thenReturn(List.of(genre1,genre2));
 
         //act
-        List<Genre> listOfOfficialGenres = _getListOfOfficialGenresController.getListOfOfficialGenres();
+        List<Genre> listOfOfficialGenres = _genreRepo.getListOfOfficialGenres();
 
         //assert
         assertNotNull(listOfOfficialGenres);
-        assertEquals(2, listOfOfficialGenres.size());
         assertFalse(listOfOfficialGenres.isEmpty());
 
     }
 
     @Test
     void test_get_list_of_official_genres_should_return_empty_list_if_no_genres_were_added() {
+        //arrange
+        _user = mock(User.class);
+        _genreRepo = mock(GenreRepo.class);
+        _getListOfOfficialGenresController = new GetListOfOfficialGenresController(_genreRepo, _user);
 
         //act
         List<Genre> listOfOfficialGenres = _getListOfOfficialGenresController.getListOfOfficialGenres();
