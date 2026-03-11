bf911de37724290d4a36b686646846998c47ddc9
Ricardo <1252005@isep.ipp.pt>
2026-03-11 11:17:25 +0000

    Closes #313 Javadoc for UserFactory class

--- src/main/java/TOPSECRET/domain/UserFactory.java
+++ src/main/java/TOPSECRET/domain/UserFactory.java
@@ -1,8 +1,21 @@
 package TOPSECRET.domain;
 
+
+/**
+ * Factory responsible for creating {@link User} instances.
+ * <p>
+ * Any exception thrown during the creation process is wrapped
+ * into an {@link NullPointerException}.
+ */
+
+
 public class UserFactory {
 
     public User createUser(Name name, Email email) {
         return new User(name, email);
+    }
+
+    public User createUserTypeB(Name name, Address address, Email email, Phone phone) {
+        return new User (name, address, email, phone);
     }
 }
