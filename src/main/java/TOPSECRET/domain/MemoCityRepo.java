25812097f53e29954575d7e38a79c3e0c9cdde16
Jose Sousa <1251994@isep.ipp.pt>
2026-04-02 19:21:20 +0100

    Refactor: Updated MemoCityRepo and tests to extendo IRepository, Refs: #478

--- src/main/java/TOPSECRET/domain/MemoCityRepo.java
+++ src/main/java/TOPSECRET/domain/MemoCityRepo.java
@@ -1,56 +1,40 @@
 package TOPSECRET.domain;
 
+import TOPSECRET.domain.valueobject.CityId;
 import TOPSECRET.domain.country.Country;
 
 import java.util.ArrayList;
 import java.util.List;
-
-/**
- * Repository for managing {@link City} instances.
- * <p>
- * Provides methods to check for the existence of a city by name and country,
- * save new cities, and retrieve all stored cities as list.copyOf.
- * </p>
- */
+import java.util.Optional;
 
 public class MemoCityRepo implements ICityRepo {
-    private final List<City> _cities;
-    private final CityFactory _cityFactory;
 
-    public MemoCityRepo(CityFactory cityFactory) {
-        _cities = new ArrayList<>();
-        _cityFactory = cityFactory;
-    }
+    private final List<City> _cities = new ArrayList<>();
 
     @Override
-    public City registerCity(String cityName, Country country) {
-        if (existsCityInACountry(cityName, country)) {
+    public City save(City city) {
+        if (containsOfIdentity(city.identity())) {
             throw new IllegalStateException("City already exists for this country");
         }
-
-        City city = _cityFactory.createCity(cityName, country);
         _cities.add(city);
         return city;
     }
 
     @Override
-    public boolean existsCityInACountry(String cityName, Country country) {
-        if (cityName == null || country == null) {
-            return false;
-        }
-        String normalized = cityName.trim();
-
-        for (City city : _cities) {
-            if (city.getName().equalsIgnoreCase(normalized) && city.getCountry().equals(country)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    @Override
-    public List<City> getAllCities() {
+    public Iterable<City> findAll() {
         return List.copyOf(_cities);
     }
 
+    @Override
+    public Optional<City> ofIdentity(CityId cityId) {
+        return _cities.stream()
+                .filter(c -> c.identity().equals(cityId))
+                .findFirst();
+    }
+
+    @Override
+    public boolean containsOfIdentity(CityId cityId) {
+        return _cities.stream()
+                .anyMatch(c -> c.identity().equals(cityId));
+    }
 }
