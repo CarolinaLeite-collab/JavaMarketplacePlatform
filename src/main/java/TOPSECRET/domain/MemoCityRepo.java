86afcf3173c6eec6209979c6c1365a2ae93c301d
Jose Sousa <1251994@isep.ipp.pt>
2026-04-07 15:19:06 +0100

    Refactor: Replace List with Map in MemoCityRepo, Refs #475

--- src/main/java/TOPSECRET/domain/MemoCityRepo.java
+++ src/main/java/TOPSECRET/domain/MemoCityRepo.java
@@ -4,38 +4,33 @@ import TOPSECRET.domain.city.City;
 import TOPSECRET.domain.repository.ICityRepo;
 import TOPSECRET.domain.valueobject.CityId;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 public class MemoCityRepo implements ICityRepo {
 
-    private final List<City> _cities = new ArrayList<>();
+    private final Map<CityId, City> _cities = new HashMap<>();
 
     @Override
     public City save(City city) {
         if (containsOfIdentity(city.identity())) {
             throw new IllegalStateException("City already exists for this country");
         }
-        _cities.add(city);
+        _cities.put(city.identity(), city);
         return city;
     }
 
     @Override
     public Iterable<City> findAll() {
-        return List.copyOf(_cities);
+        return List.copyOf(_cities.values());
     }
 
     @Override
     public Optional<City> ofIdentity(CityId cityId) {
-        return _cities.stream()
-                .filter(c -> c.identity().equals(cityId))
-                .findFirst();
+        return Optional.ofNullable(_cities.get(cityId));
     }
 
     @Override
     public boolean containsOfIdentity(CityId cityId) {
-        return _cities.stream()
-                .anyMatch(c -> c.identity().equals(cityId));
+        return _cities.containsKey(cityId);
     }
 }
