f08fec1450951e2b4c1b5bf36abd89d7ead4941e
Vivian Sandler <1252010@isep.ipp.pt>
2026-04-07 18:27:45 +0100

    refactor: move MemoPublishingCompanyRepo to persistence/mem package, ref. #423

--- src/main/java/TOPSECRET/persistence/mem/MemoPublishingCompanyRepo.java
+++ src/main/java/TOPSECRET/persistence/mem/MemoPublishingCompanyRepo.java
@@ -1,4 +1,4 @@
-package TOPSECRET.domain;
+package TOPSECRET.persistence.mem;
 
 import TOPSECRET.domain.publishingcompany.PublishingCompany;
 import TOPSECRET.domain.publishingcompany.PublishingCompanyFactory;
