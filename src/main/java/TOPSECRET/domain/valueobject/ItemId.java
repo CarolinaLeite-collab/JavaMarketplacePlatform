9ae591b5cfa37e6d7c24a8454c44b4c84915ab63
TiagoLeite1252008 <1252008@isep.ipp.pt>
2026-04-08 01:58:30 +0100

    Refactor: Create ItemId with DomainId implementation and unit tests, Refs: #509

--- src/main/java/TOPSECRET/domain/valueobject/ItemId.java
+++ src/main/java/TOPSECRET/domain/valueobject/ItemId.java
@@ -2,6 +2,37 @@ package TOPSECRET.domain.valueobject;
 
 import TOPSECRET.ddd.DomainId;
 
-public class ItemId implements DomainId {
+import java.util.Objects;
 
+public final class ItemId implements DomainId {
+
+    private final SKU _sku;
+
+    public ItemId(SKU sku) {
+
+        _sku = Objects.requireNonNull(sku,"SKU is required.");
+    }
+
+    public static ItemId generate() {
+        return new ItemId(SKU.generate());
+    }
+
+    public SKU getSku() { return _sku; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ItemId other)) return false;
+        return _sku.equals(other._sku);
+    }
+
+    @Override
+    public String toString() {
+        return _sku.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return _sku.hashCode();
+    }
 }
