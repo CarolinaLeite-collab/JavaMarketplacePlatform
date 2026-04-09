cf7a8e778877ba2e2f8c188b7d3aaf5495fd5f50
Marcelo Rocha <1251995@isep.ipp.pt>
2026-04-09 01:25:29 +0100

    refactor: updated AppraisalEntityFactory to accept PublicationTypeId and GenreId and updated units tests to reflect DDD - refs #414

--- src/main/java/TOPSECRET/domain/appraisalEntity/AppraisalEntityFactory.java
+++ src/main/java/TOPSECRET/domain/appraisalEntity/AppraisalEntityFactory.java
@@ -1,9 +1,9 @@
 package TOPSECRET.domain.appraisalEntity;
 
-import TOPSECRET.domain.publicationtype.PublicationType;
-import TOPSECRET.domain.genre.Genre;
+import TOPSECRET.domain.valueobject.AppraisalEntityId;
+import TOPSECRET.domain.valueobject.GenreId;
 import TOPSECRET.domain.valueobject.Name;
-
+import TOPSECRET.domain.valueobject.PublicationTypeId;
 import java.util.List;
 
 /**
@@ -13,7 +13,12 @@ import java.util.List;
  */
 
 public class AppraisalEntityFactory {
-    public AppraisalEntity createAppraisalEntity(Name name, List<PublicationType> publicationTypes, List<Genre> genres) {
-        return new AppraisalEntity(name, publicationTypes, genres);
+
+
+    public AppraisalEntity createAppraisalEntity(Name name, List<PublicationTypeId> publicationTypeIds, List<GenreId> genresId) {
+
+        return new AppraisalEntity(name, publicationTypeIds, genresId);
+
     }
+
 }
