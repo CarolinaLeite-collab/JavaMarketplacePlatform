68ded3737111d9dfdd9507fa1d461b5a75b913b4
Guilherme Lemes <1251991@isep.ipp.pt>
2026-04-16 18:06:00 +0100

    fix: removes incorrectly added code, refs #574

--- src/main/java/TOPSECRET/controller/GetItemsOnAuctionOfAGivenAuthorController.java
+++ src/main/java/TOPSECRET/controller/GetItemsOnAuctionOfAGivenAuthorController.java
@@ -72,26 +72,3 @@ public class GetItemsOnAuctionOfAGivenAuthorController {
     }
 
 }
-package TOPSECRET.controller;
-
-import TOPSECRET.domain.author.Author;
-import TOPSECRET.domain.repository.IAuctionRepo;
-import TOPSECRET.domain.item.Item;
-import TOPSECRET.domain.valueobject.UserId;
-
-/**
- * Controller responsible for retrieving auction items by a specific author.
- * <p>
- * This controller uses the {@link IAuctionRepo} to obtain a list of {@link Item}
- * instances that are currently on auction and were created by a given {@link Author}.
- * </p>
- */
-
-public class GetItemsOnAuctionOfAGivenAuthorController {
-
-    private final IAuctionRepo _iAuctionRepo;
-
-    public GetItemsOnAuctionOfAGivenAuthorController(IAuctionRepo iAuctionRepo, UserId buyerId) {
-        _iAuctionRepo = iAuctionRepo;
-    }
-}
