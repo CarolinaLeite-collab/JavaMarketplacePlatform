86241f4e3982e83ac377b82bd99a83b2d556cd10
edu_link <1251996@isep.ipp.pt>
2026-04-16 14:52:34 +0100

    Implement the findAllKeys method in the IListOfItemsRepo #575

--- src/main/java/TOPSECRET/persistence/mem/MemoListOfItemsRepo.java
+++ src/main/java/TOPSECRET/persistence/mem/MemoListOfItemsRepo.java
@@ -7,10 +7,7 @@ import TOPSECRET.domain.valueobject.GenreId;
 import TOPSECRET.domain.valueobject.ListOfItemsId;
 import TOPSECRET.domain.valueobject.UserId;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -61,6 +58,11 @@ public class MemoListOfItemsRepo implements IListOfItemsRepo {
     @Override
     public Iterable<ListOfItems> findAll() {
         return List.copyOf(_data.values());
+    }
+
+    public ArrayList<ListOfItemsId> findAllKeys() {
+
+        return new ArrayList<>(_data.keySet());
     }
 
     @Override
