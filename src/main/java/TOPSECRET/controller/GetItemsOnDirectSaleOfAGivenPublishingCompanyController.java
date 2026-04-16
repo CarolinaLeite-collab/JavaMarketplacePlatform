64b870dbf3149695a2e4c1c9d91f3ac8b5329d94
rafaelar <1252002@isep.ipp.pt>
2026-04-16 17:51:06 +0100

    refactor: updates GetItemsOnDirectSaleOfAGivenPublishingCompanyController tests and methods to follow DDD conventions, refs #572

--- src/main/java/TOPSECRET/controller/GetItemsOnDirectSaleOfAGivenPublishingCompanyController.java
+++ src/main/java/TOPSECRET/controller/GetItemsOnDirectSaleOfAGivenPublishingCompanyController.java
@@ -1,9 +1,16 @@
 package TOPSECRET.controller;
 
-import TOPSECRET.domain.repository.IDirectSaleRepo;
+import TOPSECRET.domain.directsale.DirectSale;
+import TOPSECRET.domain.edition.Edition;
+import TOPSECRET.domain.publication.Publication;
+import TOPSECRET.domain.repository.*;
 import TOPSECRET.domain.item.Item;
 import TOPSECRET.domain.publishingcompany.PublishingCompany;
-import TOPSECRET.domain.valueobject.UserId;
+import TOPSECRET.domain.valueobject.*;
+import TOPSECRET.persistence.mem.MemoPublishingCompanyRepo;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -17,9 +24,43 @@ import TOPSECRET.domain.valueobject.UserId;
 public class GetItemsOnDirectSaleOfAGivenPublishingCompanyController {
 
     private final IDirectSaleRepo _iDirectSaleRepo;
+    private IItemRepo _iItemRepo;
+    private IPublishingCompanyRepo _iPublishingCompanyRepo;
+    public IEditionRepo _iEditionRepo;
 
-    public GetItemsOnDirectSaleOfAGivenPublishingCompanyController(IDirectSaleRepo directSaleRepo, UserId buyerId) {
-
+    public GetItemsOnDirectSaleOfAGivenPublishingCompanyController(IPublishingCompanyRepo pcr, IItemRepo ir, IEditionRepo er, IDirectSaleRepo directSaleRepo, UserId buyerId) {
+        _iPublishingCompanyRepo = pcr;
+        _iItemRepo = ir;
+        _iEditionRepo = er;
         _iDirectSaleRepo = directSaleRepo;
+    }
+
+    public Iterable<PublishingCompanyId> findAllKeys(){
+        Iterable<PublishingCompanyId> publishingCompanyIds = _iPublishingCompanyRepo.findAllKeys();
+
+        return publishingCompanyIds;
+    }
+
+    public List<ItemId> getDirectSaleItemsByPublishingCompany(PublishingCompanyId publishingCompanyId) {
+
+        Iterable<DirectSale> directSales = _iDirectSaleRepo.findAll();
+        List<ItemId> listOfItemsOnDirectSaleByPublishingCompany = new ArrayList<>();
+
+        for(DirectSale directSale: directSales){
+            List<ItemId> itemIds = directSale.getItemsId();
+
+            for(ItemId itemId: itemIds) {
+                Item item = _iItemRepo.ofIdentity(itemId).orElseThrow(() -> new IllegalStateException("Item not found"));
+
+                EditionId editionId = item.getEditionId();
+
+                Edition edition = _iEditionRepo.ofIdentity(editionId).orElseThrow(() -> new IllegalStateException("Edition not found"));
+
+                if (edition.isByPublishingCompanyId(publishingCompanyId)) {
+                    listOfItemsOnDirectSaleByPublishingCompany.add(itemId);
+                }
+            }
+        }
+        return listOfItemsOnDirectSaleByPublishingCompany;
     }
 }
