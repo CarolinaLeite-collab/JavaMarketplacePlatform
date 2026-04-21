51c7c2a5e725d0d2b8d779d54439dc3229d6d968
Carolina Leite <1251987@isep.ipp.pt>
2026-04-21 14:20:19 +0100

    feat(valueobject): add NoIdentifier value object with corresponding unit tests (#591)

---
+++ src/main/java/MITELOVERS/domain/valueobject/NoIdentifier.java
@@ -0,0 +1,18 @@
+package MITELOVERS.domain.valueobject;
+
+import MITELOVERS.ddd.ValueObject;
+
+public class NoIdentifier implements Identifier, ValueObject {
+
+    private final String _noIdentifier;
+
+    public NoIdentifier() {
+        _noIdentifier ="no identifier";
+    }
+
+    @Override
+    public String getIdentifier(){
+        return _noIdentifier;
+    }
+
+}
