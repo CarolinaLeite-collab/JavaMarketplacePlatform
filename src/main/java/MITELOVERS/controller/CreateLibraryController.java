880f5f4e0a0d9d6d977488e86b88cea2e44ce7df
Vitor Barros <1252009@isep.ipp.pt>
2026-04-22 23:22:01 +0100

    refactor: updated orchestration on CreateLibraryController class and refactored tests, refs #605

--- src/main/java/MITELOVERS/controller/CreateLibraryController.java
+++ src/main/java/MITELOVERS/controller/CreateLibraryController.java
@@ -1,6 +1,7 @@
 package MITELOVERS.controller;
 
 import MITELOVERS.domain.library.Library;
+import MITELOVERS.domain.library.LibraryFactory;
 import MITELOVERS.domain.repository.ILibraryRepo;
 import MITELOVERS.domain.valueobject.UserId;
 
@@ -20,16 +21,26 @@ import MITELOVERS.domain.valueobject.UserId;
 public class CreateLibraryController {
 
     private final ILibraryRepo _iLibraryRepo;
+    private final LibraryFactory _libraryFactory;
 
-    public CreateLibraryController(ILibraryRepo lr, UserId userId){
+    public CreateLibraryController(ILibraryRepo lr, LibraryFactory libraryFactory, UserId userId){
 
         _iLibraryRepo =lr;
+        _libraryFactory = libraryFactory;
 
     }
 
     public boolean createLibrary(UserId userId){
 
-        _iLibraryRepo.addLibrary(userId);
+        Library myLibrary = _libraryFactory.createLibrary(userId);
+
+        if (_iLibraryRepo.containsOfIdentity(myLibrary.identity())) {
+
+            throw new IllegalStateException("User already has a library!");
+
+        }
+
+        _iLibraryRepo.save(myLibrary);
 
         return true;
 
