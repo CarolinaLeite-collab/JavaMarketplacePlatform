fa93506a913ea824cb8ff78cf7aa580ea382850e
Carolina Leite <1251987@isep.ipp.pt>
2026-04-26 14:31:38 +0100

    feat: create DirectSaleAssembler and add unit tests (#692)

--- src/main/java/MITELOVERS/persistence/jpa/assembler/DirectSaleAssembler.java
+++ src/main/java/MITELOVERS/persistence/jpa/assembler/DirectSaleAssembler.java
@@ -1,4 +1,69 @@
 package MITELOVERS.persistence.jpa.assembler;
 
+
+import MITELOVERS.domain.directsale.DirectSale;
+import MITELOVERS.domain.directsale.DirectSaleFactory;
+import MITELOVERS.domain.valueobject.Currency;
+import MITELOVERS.domain.valueobject.DirectSaleId;
+import MITELOVERS.domain.valueobject.ItemId;
+import MITELOVERS.domain.valueobject.Price;
+import MITELOVERS.persistence.jpa.datamodel.DirectSaleDataModel;
+import MITELOVERS.persistence.jpa.datamodel.PriceDataModel;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.time.Period;
+
 public class DirectSaleAssembler {
+    private final DirectSaleFactory factory;
+
+    public DirectSaleAssembler(DirectSaleFactory factory){
+        this.factory = factory;
+    }
+
+    public DirectSaleDataModel domain2DM(DirectSale directSale){
+
+        DirectSaleDataModel dm = new DirectSaleDataModel(
+                directSale.identity().toString(),
+                mapItemsToString(directSale.getItemsId()),
+                new PriceDataModel(
+                        directSale.getPrice().getValue(),
+                        directSale.getPrice().getCurrency().toString()),
+                directSale.getTimeLimit().toString());
+
+        return dm;
+    }
+
+    public DirectSale DM2Domain(DirectSaleDataModel dm){
+
+        DirectSale directSale = factory.createDirectSale(
+                new DirectSaleId(dm.getDirectSaleId()),
+                mapStringToItems(dm.getItemsId()),
+                new Price(
+                        dm.getPrice().getValue(),
+                        Currency.valueOf(dm.getPrice().getCurrency())),
+                Period.parse(dm.getTimeLimit())
+        );
+
+        return directSale;
+    }
+
+    private List<String> mapItemsToString(List<ItemId> itemsId){
+        List<String> result = new ArrayList<>();
+
+        for (ItemId item : itemsId) {
+            result.add(item.toString());
+        }
+        return result;
+    }
+
+    private List<ItemId> mapStringToItems(List<String> itemsId){
+        List<ItemId> result = new ArrayList<>();
+
+        for (String item : itemsId) {
+            result.add(new ItemId(item));
+        }
+        return result;
+    }
+
 }
---
+++ src/test/java/MITELOVERS/persistence/jpa/assembler/DirectSaleAssemblerTest.java
@@ -0,0 +1,98 @@
+package MITELOVERS.persistence.jpa.assembler;
+
+import MITELOVERS.domain.directsale.DirectSale;
+import MITELOVERS.domain.directsale.DirectSaleFactory;
+import MITELOVERS.domain.valueobject.Currency;
+import MITELOVERS.domain.valueobject.DirectSaleId;
+import MITELOVERS.domain.valueobject.ItemId;
+import MITELOVERS.domain.valueobject.Price;
+import MITELOVERS.persistence.jpa.datamodel.DirectSaleDataModel;
+import MITELOVERS.persistence.jpa.datamodel.PriceDataModel;
+import org.junit.jupiter.api.Test;
+
+import java.time.Period;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class DirectSaleAssemblerTest {
+
+    @Test
+    void directSaleAssemblerConstructorTest(){
+        //Arrange
+        DirectSaleFactory factory = mock(DirectSaleFactory.class);
+
+        //Act
+        //SUT
+        new DirectSaleAssembler(factory);
+    }
+
+    @Test
+    void shouldConvertDomainToDataModel() {
+        //Arrange
+        DirectSaleId id = mock(DirectSaleId.class);
+        ItemId item = mock(ItemId.class);
+        when(id.toString()).thenReturn("DS1");
+
+        Price price = mock(Price.class);
+        when(price.getValue()).thenReturn(10.0);
+        when(price.getCurrency()).thenReturn(Currency.EUR);
+
+        Period period = Period.ofDays(5);
+
+        DirectSaleFactory factory = mock(DirectSaleFactory.class);
+
+        DirectSale directSale = mock(DirectSale.class);
+        when(directSale.identity()).thenReturn(id);
+        when(directSale.getItemsId()).thenReturn(List.of(item));
+        when(directSale.getPrice()).thenReturn(price);
+        when(directSale.getTimeLimit()).thenReturn(period);
+
+        String expectedResult = period.toString();
+
+        //SUT
+        DirectSaleAssembler assembler = new DirectSaleAssembler(factory);
+
+        //Act
+        DirectSaleDataModel dm = assembler.domain2DM(directSale);
+
+        //Assert
+        assertEquals(expectedResult,dm.getTimeLimit());
+
+    }
+
+    @Test
+    void shouldConvertDataModelToDomain() {
+        //Arrange
+        DirectSale expected = mock(DirectSale.class);
+
+        PriceDataModel priceDM = mock(PriceDataModel.class);
+        when(priceDM.getValue()).thenReturn(10.0);
+        when(priceDM.getCurrency()).thenReturn("EUR");
+
+        DirectSaleDataModel dm = mock(DirectSaleDataModel.class);
+        when(dm.getDirectSaleId()).thenReturn("DS1");
+        when(dm.getItemsId()).thenReturn(List.of("ABC123DEF0"));
+        when(dm.getPrice()).thenReturn(priceDM);
+        when(dm.getTimeLimit()).thenReturn("P5D");
+
+        DirectSaleFactory factory = mock(DirectSaleFactory.class);
+        when(factory.createDirectSale(
+                new DirectSaleId("DS1"),
+                List.of(new ItemId("ABC123DEF0")),
+                new Price(10.0, Currency.EUR),
+                Period.parse("P5D")
+        )).thenReturn(expected);
+
+        //SUT
+        DirectSaleAssembler assembler = new DirectSaleAssembler(factory);
+
+        //Act
+        DirectSale result = assembler.DM2Domain(dm);
+
+        //Assert
+        assertSame(expected, result);
+    }
+}
