aa09b90f91532d8799cf74b2e975d775f693f265
filipamesquita <1251990@isep.ipp.pt>
2026-04-26 14:48:50 +0100

    refactor: add @Generated annotation to UserDataModel to exclude Lombok-generated code from PIT refs #662

--- src/main/java/MITELOVERS/persistence/jpa/datamodel/UserDataModel.java
+++ src/main/java/MITELOVERS/persistence/jpa/datamodel/UserDataModel.java
@@ -3,6 +3,7 @@ package MITELOVERS.persistence.jpa.datamodel;
 import MITELOVERS.domain.user.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Generated;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
  * allowing its persistence in a database.
  */
 
+@Generated
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
