97d9d465266b02f346263f878333bcf60c7ae13f
mcosta <1251998@isep.ipp.pt>
2026-04-26 23:42:50 +0100

    refactor: add @generated annotation to AuthorDataModel to exclude Lombok-generated code from PIT refs #680

--- src/main/java/MITELOVERS/persistence/jpa/datamodel/AuthorDataModel.java
+++ src/main/java/MITELOVERS/persistence/jpa/datamodel/AuthorDataModel.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Generated;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -14,6 +15,7 @@ import lombok.NoArgsConstructor;
  * allowing its persistence in a database.
  */
 
+@Generated
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
