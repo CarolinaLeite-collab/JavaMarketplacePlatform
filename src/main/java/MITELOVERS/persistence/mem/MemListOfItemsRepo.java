7990997c177b1e0018a1014bf9f9291d28804eb4
edu_link <1251996@isep.ipp.pt>
2026-04-26 16:31:06 +0100

    fix: Removed unused imports, #677

--- src/main/java/MITELOVERS/persistence/mem/MemListOfItemsRepo.java
+++ src/main/java/MITELOVERS/persistence/mem/MemListOfItemsRepo.java
@@ -2,9 +2,8 @@ package MITELOVERS.persistence.mem;
 
 import MITELOVERS.domain.listofitems.ListOfItems;
 import MITELOVERS.domain.repository.IListOfItemsRepo;
-import MITELOVERS.domain.valueobject.GenreId;
 import MITELOVERS.domain.valueobject.ListOfItemsId;
-import MITELOVERS.domain.valueobject.UserId;
+
 
 import java.util.*;
 
