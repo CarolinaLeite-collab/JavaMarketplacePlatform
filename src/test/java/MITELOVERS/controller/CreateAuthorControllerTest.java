62bd14d98479c42003d889d825b45ec56dcdc225
mcosta <1251998@isep.ipp.pt>
2026-04-30 18:03:45 +0100

    fix: move jpa profile activation to application.properties, ref #426

--- src/test/java/MITELOVERS/controller/CreateAuthorControllerTest.java
+++ src/test/java/MITELOVERS/controller/CreateAuthorControllerTest.java
@@ -16,7 +16,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
-@ActiveProfiles("jpa")
 class CreateAuthorControllerTest {
 
     @MockBean
