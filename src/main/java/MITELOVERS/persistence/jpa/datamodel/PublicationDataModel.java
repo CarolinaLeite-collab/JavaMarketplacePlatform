5468779663639f4e643fbbbffddd16b24eeb6e3b
rafaelar <1252002@isep.ipp.pt>
2026-05-02 10:14:33 +0100

    fix: removes unique tag from unnecessary fields, refs #731

--- src/main/java/MITELOVERS/persistence/jpa/datamodel/PublicationDataModel.java
+++ src/main/java/MITELOVERS/persistence/jpa/datamodel/PublicationDataModel.java
@@ -14,14 +14,14 @@ import lombok.*;
 @Table(name = "Publications")
 public class PublicationDataModel {
     @Id
-    @Column(name = "id", nullable = false, unique = true)
+    @Column(name = "id", nullable = false)
     private String id;
-    @Column(name = "title", nullable = false, unique = true)
+    @Column(name = "title", nullable = false)
     private String Title;
-    @Column(name = "authorId", nullable = false, unique = true)
+    @Column(name = "authorId", nullable = false)
     private String AuthorId;
-    @Column(name = "releaseYear", nullable = false, unique = true)
+    @Column(name = "releaseYear", nullable = false)
     private String ReleaseYear;
-    @Column(name = "genreId", nullable = false, unique = true)
+    @Column(name = "genreId", nullable = false)
     private String GenreId;
 }
