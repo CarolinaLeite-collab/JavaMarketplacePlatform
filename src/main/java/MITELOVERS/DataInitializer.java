80492cc94d29cafbc78092cfdaab36a5e3d6f3bb
Carolina Leite <1251987@isep.ipp.pt>
2026-05-06 11:30:13 +0100

    feat: add DataInitializer for DirectSale bootstrap data seeding #688

--- src/main/java/MITELOVERS/DataInitializer.java
+++ src/main/java/MITELOVERS/DataInitializer.java
@@ -1,8 +1,14 @@
 package MITELOVERS;
 
+import MITELOVERS.domain.directsale.DirectSale;
+import MITELOVERS.domain.directsale.DirectSaleFactory;
 import MITELOVERS.domain.genre.Genre;
 import MITELOVERS.domain.genre.GenreFactory;
+import MITELOVERS.domain.repository.IDirectSaleRepo;
 import MITELOVERS.domain.repository.IGenreRepo;
+import MITELOVERS.domain.valueobject.Currency;
+import MITELOVERS.domain.valueobject.ItemId;
+import MITELOVERS.domain.valueobject.Price;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -10,8 +16,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
-import MITELOVERS.domain.valueobject.GenreId;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+
 
 @Component
 @Profile("bootstrap")
@@ -22,47 +31,34 @@ public class DataInitializer {
     @Bean
     public CommandLineRunner initData(
             IGenreRepo genreRepo,
-            GenreFactory genreFactory) {
+            GenreFactory genreFactory,
+            DirectSaleFactory directSaleFactory,
+            IDirectSaleRepo directSaleRepo) {
 
         return args -> {
 
+            //Create and save a few objects
             // -------------------------------------------------------
             // Genre
-            // Create and save a few Genres
             Genre genre1 = genreFactory.createGenre("Fiction");
             genreRepo.save(genre1);
-
             Genre genre2 = genreFactory.createGenre("Non-Fiction");
             genreRepo.save(genre2);
-
             Genre genre3 = genreFactory.createGenre("Science Fiction");
             genreRepo.save(genre3);
 
-            // Fetch and log all genres
-            log.info("Genres found with findAll():");
-            log.info("-------------------------------");
-            for (Genre genre : genreRepo.findAll()) {
-                log.info(genre.toString());
-            }
-            log.info("");
+            //DirectSale
+            ItemId itemId = new ItemId("ABCDEF1234");
+            List<ItemId> itemsId = new ArrayList<>();
+            itemsId.add(itemId);
+            Price price = new Price(10, Currency.EUR);
+            Duration timeLimit = Duration.ofDays(7);
 
-            // Fetch an individual genre by ID
-            GenreId targetGenreId = genre1.identity();
-            Optional<Genre> opGenre = genreRepo.ofIdentity(targetGenreId);
-            if (opGenre.isPresent()) {
-                log.info("Genre found with ofIdentity(genre1.identity()):");
-                log.info("--------------------------------");
-                log.info(opGenre.get().toString());
-                log.info("");
-            }
+            DirectSale directSale1 = directSaleFactory.createDirectSale(itemsId, price, timeLimit);
+            directSaleRepo.save(directSale1);
 
-            // Log all Genre keys
-            log.info("All Genre IDs:");
-            log.info("-------------------------------");
-            for (GenreId id : genreRepo.findAllKeys()) {
-                log.info(id.toString());
-            }
         };
     }
+
 
 }
