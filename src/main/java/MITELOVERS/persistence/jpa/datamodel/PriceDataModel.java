7a7227ddb7d97914c4d0cf06fa0dc0eeca99cbb5
Carolina Leite <1251987@isep.ipp.pt>
2026-05-07 16:10:40 +0100

    Adds @Generated notation to PriceDataModel #690

--- src/main/java/MITELOVERS/persistence/jpa/datamodel/PriceDataModel.java
+++ src/main/java/MITELOVERS/persistence/jpa/datamodel/PriceDataModel.java
@@ -1,15 +1,13 @@
 package MITELOVERS.persistence.jpa.datamodel;
 
 import jakarta.persistence.Embeddable;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 /**
  * Data model object representing {@link MITELOVERS.domain.valueobject.Price} value object,
  * enabling its persistence as part of a JPA entity.
  */
+@Generated
 @EqualsAndHashCode
 @Getter
 @NoArgsConstructor
