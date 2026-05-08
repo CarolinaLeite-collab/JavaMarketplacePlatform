3dfff3059bf7e6764c8f077cf9fcb54a186841ef
Ricardo <1252005@isep.ipp.pt>
2026-05-08 15:14:18 +0100

    refs #650 creation of SpringData interface so it can use CRUD operations

---
+++ src/main/java/MITELOVERS/persistence/jpa/springdata/ICountrySpringDataRepo.java
@@ -0,0 +1,15 @@
+package MITELOVERS.persistence.jpa.springdata;
+
+import MITELOVERS.persistence.jpa.datamodel.CountryDataModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for {@link CountryDataModel} persistence.
+ * Provides standard CRUD operations with {@link String} as the ID type.
+ */
+
+public interface ICountrySpringDataRepo extends JpaRepository<CountryDataModel, String> {
+
+
+}
+
