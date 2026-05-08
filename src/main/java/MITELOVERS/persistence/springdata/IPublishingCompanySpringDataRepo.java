178e1d8d285709c03b453412912aef5e820ccc1f
TiagoLeite1252008 <1252008@isep.ipp.pt>
2026-05-08 17:55:13 +0100

    Javadoc documentation added, refs #752

--- src/main/java/MITELOVERS/persistence/springdata/IPublishingCompanySpringDataRepo.java
+++ src/main/java/MITELOVERS/persistence/springdata/IPublishingCompanySpringDataRepo.java
@@ -3,5 +3,13 @@ package MITELOVERS.persistence.springdata;
 import MITELOVERS.persistence.jpa.datamodel.PublishingCompanyDataModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Spring Data repository responsible for managing
+ * {@link PublishingCompanyDataModel} persistence operations.
+ *
+ * Provides CRUD operations and database access for
+ * publishing company data models.
+ */
+
 public interface IPublishingCompanySpringDataRepo extends JpaRepository<PublishingCompanyDataModel, String> {
 }
