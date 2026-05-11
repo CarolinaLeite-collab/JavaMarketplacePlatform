f0247e20cdba400fad02e21dbd74c7fbf3c62917
gui1251991 <1251991@isep.ipp.pt>
2026-05-11 22:15:10 +0100

    refactor: add PriceDataModel and PriceConverter to support AuctionAssembler price persistence mapping #814

---
+++ src/main/java/MITELOVERS/persistence/jpa/datamodel/PriceDataModel.java
@@ -0,0 +1,20 @@
+package MITELOVERS.persistence.jpa.datamodel;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * Data model object representing {@link MITELOVERS.domain.valueobject.Price} value object,
+ * enabling its persistence as part of a JPA entity.
+ */
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PriceDataModel {
+    private double numericValue;
+    private String currency;
+
+}
