acd7779875d0d6a30cf8702d40ad2f6236fbdf8c
Marcelo Rocha <1251995@isep.ipp.pt>
2026-05-13 17:40:13 +0100

    refactor: update IPublicationTypeSpringDataRepo to extend JpaRepository - refs #707

--- src/main/java/MITELOVERS/persistence/springdata/IPublicationTypeSpringDataRepo.java
+++ src/main/java/MITELOVERS/persistence/springdata/IPublicationTypeSpringDataRepo.java
@@ -1,13 +1,13 @@
 package MITELOVERS.persistence.springdata;
 
 import MITELOVERS.persistence.jpa.datamodel.PublicationTypeDataModel;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * Spring Data repository for {@link PublicationTypeDataModel}.
  * <p>
  */
 
-public interface IPublicationTypeSpringDataRepo extends CrudRepository<PublicationTypeDataModel, String> {
+public interface IPublicationTypeSpringDataRepo extends JpaRepository<PublicationTypeDataModel, String> {
 
 }
