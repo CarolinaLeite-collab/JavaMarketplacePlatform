385ddbf36c5ece97e75b391775a50b92a7a77d44
Vivian Sandler <1252010@isep.ipp.pt>
2026-05-13 03:19:07 +0100

    refactor: add @ Repository annotation, allow PublicationSaleAuctionController tests to pass, ref. #862

--- src/main/java/MITELOVERS/domain/repository/IAuctionRepo.java
+++ src/main/java/MITELOVERS/domain/repository/IAuctionRepo.java
@@ -3,7 +3,9 @@ package MITELOVERS.domain.repository;
 import MITELOVERS.ddd.IRepository;
 import MITELOVERS.domain.auction.Auction;
 import MITELOVERS.domain.valueobject.AuctionId;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IAuctionRepo extends IRepository<AuctionId, Auction> {
 
 }
