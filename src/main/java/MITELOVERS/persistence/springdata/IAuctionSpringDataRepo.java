2178e0fcc4d5668494c0c8bf0ccba5587fb19a67
edu_link <1251996@isep.ipp.pt>
2026-05-14 21:53:03 +0100

    task: Added the necessary methods to IAuctionRepo and IAuctionSpringDataRepo #869

--- src/main/java/MITELOVERS/persistence/springdata/IAuctionSpringDataRepo.java
+++ src/main/java/MITELOVERS/persistence/springdata/IAuctionSpringDataRepo.java
@@ -1,7 +1,16 @@
 package MITELOVERS.persistence.springdata;
 
 import MITELOVERS.persistence.jpa.datamodel.AuctionDataModel;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
+@Profile("jpa")
 public interface IAuctionSpringDataRepo extends JpaRepository<AuctionDataModel, String> {
+
+    List<AuctionDataModel> findAllByItemsIdOrderByAuctionEndDateAsc(List<String> itemsId);
+
 }
