7f2bdeb000de00cf07b40b9bd6d8fb5197ee9bf7
edu_link <1251996@isep.ipp.pt>
2026-05-14 13:05:58 +0100

    fix: Added annotation to LibraryFactory, #889

--- src/main/java/MITELOVERS/domain/library/LibraryFactory.java
+++ src/main/java/MITELOVERS/domain/library/LibraryFactory.java
@@ -3,6 +3,7 @@ package MITELOVERS.domain.library;
 import MITELOVERS.domain.valueobject.ItemId;
 import MITELOVERS.domain.valueobject.LibraryId;
 import MITELOVERS.domain.valueobject.UserId;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
@@ -19,6 +20,8 @@ import java.util.List;
  * (such as non-null constraints) are enforced by the {@link Library} constructor.
  * </p>
  */
+
+@Component
 public class LibraryFactory {
 
     public Library createLibrary(UserId userId) {
