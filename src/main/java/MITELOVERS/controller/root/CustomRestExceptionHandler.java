7efca27e83da93a905ec18c1e7110205f59a2c6f
filipamesquita <1251990@isep.ipp.pt>
2026-05-24 22:20:22 +0100

    feat: add CustomRestExceptionHandler for global exception handling and unit tests refs #932

---
+++ src/main/java/MITELOVERS/controller/root/CustomRestExceptionHandler.java
@@ -0,0 +1,31 @@
+package MITELOVERS.controller.root;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class CustomRestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<Object> handleIllegalState(IllegalStateException ex) {
+        ApiError apiError = new ApiError(
+                HttpStatus.NOT_FOUND,
+                ex.getLocalizedMessage(),
+                ex.getMessage()
+        );
+        return new ResponseEntity<>(apiError, apiError.getStatus());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Object> handleIllegalArgument(IllegalArgumentException ex) {
+        ApiError apiError = new ApiError(
+                HttpStatus.BAD_REQUEST,
+                ex.getLocalizedMessage(),
+                ex.getMessage()
+        );
+        return new ResponseEntity<>(apiError, apiError.getStatus());
+    }
+}
