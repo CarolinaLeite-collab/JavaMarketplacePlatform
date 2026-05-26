04b8e825ca0dbda6df754078caa7fc8fd7e0c803
Carolina Leite <carolinaleite@iMac-3.local>
2026-05-26 18:21:32 +0100

    feat: refactor PublicationService to return PublicationResponseDTO and update service tests #925

--- src/main/java/MITELOVERS/applicationservices/PublicationService.java
+++ src/main/java/MITELOVERS/applicationservices/PublicationService.java
@@ -1,15 +1,22 @@
 package MITELOVERS.services;
 
+import MITELOVERS.domain.author.Author;
+import MITELOVERS.domain.genre.Genre;
 import MITELOVERS.domain.publication.Publication;
 import MITELOVERS.domain.publication.PublicationFactory;
+import MITELOVERS.domain.repository.IAuthorRepo;
 import MITELOVERS.domain.repository.IGenreRepo;
 import MITELOVERS.domain.repository.IPublicationRepo;
 import MITELOVERS.domain.valueobject.AuthorId;
 import MITELOVERS.domain.valueobject.GenreId;
 import MITELOVERS.domain.valueobject.Title;
+import MITELOVERS.dto.PublicationResponseDTO;
+import MITELOVERS.mapper.PublicationResponseDTOMapper;
 import org.springframework.stereotype.Service;
 
 import java.time.Year;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Objects;
 
@@ -19,37 +26,83 @@ public class PublicationService {
     private IPublicationRepo _iPublicationRepo;
     private PublicationFactory _publicationFactory;
     private IGenreRepo _iGenreRepo;
+    private final IAuthorRepo _iAuthorRepo;
+    private final PublicationResponseDTOMapper _publicationResponseDTOMapper;
 
     public PublicationService(IPublicationRepo iPublicationRepo,
                               PublicationFactory publicationFactory,
-                              IGenreRepo iGenreRepo) {
+                              IGenreRepo iGenreRepo,
+                              IAuthorRepo iAuthorRepo,
+                              PublicationResponseDTOMapper mapper) {
 
         _iPublicationRepo = Objects.requireNonNull(iPublicationRepo, "PublicationRepo is required");
         _publicationFactory = Objects.requireNonNull(publicationFactory, "PublicationFactory is required");
         _iGenreRepo = Objects.requireNonNull(iGenreRepo, "GenreRepo is required");
+        _iAuthorRepo = Objects.requireNonNull(iAuthorRepo, "AuthorRepo is required");
+        _publicationResponseDTOMapper = Objects.requireNonNull(mapper, "PublicationDTOAssembler is required");
     }
 
 
-    public Publication registerPublication(Title title,
-                                           AuthorId authorId,
-                                           Year releaseYear,
-                                           GenreId genreId) {
+    public PublicationResponseDTO registerPublication(Title title,
+                                                      AuthorId authorId,
+                                                      Year releaseYear,
+                                                      GenreId genreId
+    ) {
+
+        Author author = _iAuthorRepo.ofIdentity(authorId)
+                .orElseThrow(() -> new NoSuchElementException(
+                        "Author does not exist in the repository"
+                ));
+
+        Genre genre = _iGenreRepo.ofIdentity(genreId)
+                .orElseThrow(() -> new NoSuchElementException(
+                        "Genre does not exist in the repository"
+                ));
 
         Publication newPublication = _publicationFactory.createPublication(title, authorId, releaseYear, genreId);
-
-        if (!_iGenreRepo.containsOfIdentity(genreId)) {
-            throw new NoSuchElementException(
-                    "Genre does not exist in the repository"
-            );
-        }
 
         if (_iPublicationRepo.containsOfIdentity(newPublication.identity())){
             throw new IllegalStateException(
                     "Publication already exists in the repository");
         }
 
-        return _iPublicationRepo.save(newPublication);
+        Publication savedPublication = _iPublicationRepo.save(newPublication);
+
+        return _publicationResponseDTOMapper.toResponseDTO(
+                savedPublication,
+                author,
+                genre
+        );
     }
 
+    public List<PublicationResponseDTO> getAllPublications() {
+
+        Iterable<Publication> publications = _iPublicationRepo.findAll();
+
+        List<PublicationResponseDTO> response = new ArrayList<>();
+
+        for (Publication publication : publications) {
+
+            Author author = _iAuthorRepo.ofIdentity(publication.getAuthorId())
+                    .orElseThrow(() -> new NoSuchElementException(
+                            "Author does not exist in the repository"
+                    ));
+
+            Genre genre = _iGenreRepo.ofIdentity(publication.getGenreId())
+                    .orElseThrow(() -> new NoSuchElementException(
+                            "Genre does not exist in the repository"
+                    ));
+
+            response.add(
+                    _publicationResponseDTOMapper.toResponseDTO(
+                            publication,
+                            author,
+                            genre
+                    )
+            );
+        }
+
+        return response;
+    }
 }
 
--- src/test/java/MITELOVERS/applicationservices/PublicationServiceTest.java
+++ src/test/java/MITELOVERS/applicationservices/PublicationServiceTest.java
@@ -1,12 +1,18 @@
 package MITELOVERS.applicationservices;
 
+import MITELOVERS.domain.author.Author;
+import MITELOVERS.domain.genre.Genre;
 import MITELOVERS.domain.publication.Publication;
 import MITELOVERS.domain.publication.PublicationFactory;
+import MITELOVERS.domain.repository.IAuthorRepo;
 import MITELOVERS.domain.repository.IGenreRepo;
 import MITELOVERS.domain.repository.IPublicationRepo;
 import MITELOVERS.domain.valueobject.AuthorId;
 import MITELOVERS.domain.valueobject.GenreId;
+import MITELOVERS.domain.valueobject.PublicationId;
 import MITELOVERS.domain.valueobject.Title;
+import MITELOVERS.dto.PublicationResponseDTO;
+import MITELOVERS.mapper.PublicationResponseDTOMapper;
 import MITELOVERS.services.PublicationService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,6 +22,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.Year;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -38,10 +45,20 @@ class PublicationServiceTest {
     @Mock
     IGenreRepo _iGenreRepoDouble;
 
+    @Mock
+    IAuthorRepo _iAuthorRepoDouble;
+
+    @Mock
+    PublicationResponseDTOMapper _publicationResponseDTOMapper;
+
     String repoExceptionMessage = "PublicationRepo is required";
     String factoryExceptionMessage = "PublicationFactory is required";
+    String genreRepoExceptionMessage = "GenreRepo is required";
+    String authorRepoExceptionMessage = "AuthorRepo is required";
+    String mapperExceptionMessage = "PublicationDTOAssembler is required";
     String publicationAlreadyExistsExceptionMessage = "Publication already exists in the repository";
     String genreIdDoesntExistExceptionMessage = "Genre does not exist in the repository";
+    String authorIdDoesntExistExceptionMessage = "Author does not exist in the repository";
 
     @Test
     void registerPublicationCallsRepoWithCorrectArguments() {
@@ -52,43 +69,176 @@ class PublicationServiceTest {
         AuthorId authorIdDouble = mock(AuthorId.class);
         GenreId genreIdDouble = mock(GenreId.class);
         Publication publicationDouble = mock(Publication.class);
+        PublicationId publicationIdDouble = mock(PublicationId.class);
+        Author authorDouble = mock(Author.class);
+        Genre genreDouble = mock(Genre.class);
+        PublicationResponseDTO responseDTODouble = mock(PublicationResponseDTO.class);
 
-        when(_publicationFactoryDouble.createPublication(titleDouble, authorIdDouble, yearDouble, genreIdDouble))
-                .thenReturn(publicationDouble);
-        when(_iPublicationRepoDouble.containsOfIdentity(publicationDouble.identity()))
+        when(_iAuthorRepoDouble.ofIdentity(authorIdDouble))
+                .thenReturn(Optional.of(authorDouble));
+
+        when(_iGenreRepoDouble.ofIdentity(genreIdDouble))
+                .thenReturn(Optional.of(genreDouble));
+
+        when(_publicationFactoryDouble.createPublication(
+                titleDouble,
+                authorIdDouble,
+                yearDouble,
+                genreIdDouble
+        )).thenReturn(publicationDouble);
+
+        when(publicationDouble.identity())
+                .thenReturn(publicationIdDouble);
+
+        when(_iPublicationRepoDouble.containsOfIdentity(publicationIdDouble))
                 .thenReturn(false);
-        when(_iGenreRepoDouble.containsOfIdentity(genreIdDouble))
-                .thenReturn(true);
+
         when(_iPublicationRepoDouble.save(publicationDouble))
                 .thenReturn(publicationDouble);
 
+        when(_publicationResponseDTOMapper.toResponseDTO(
+                publicationDouble,
+                authorDouble,
+                genreDouble
+        )).thenReturn(responseDTODouble);
+
         //Act
-        Publication result = _service.registerPublication(titleDouble, authorIdDouble, yearDouble, genreIdDouble);
+        PublicationResponseDTO result = _service.registerPublication(
+                titleDouble,
+                authorIdDouble,
+                yearDouble,
+                genreIdDouble
+        );
 
         //Assert
-        assertSame(publicationDouble, result);
+        assertSame(responseDTODouble, result);
+    }
+
+    @Test
+    void constructorThrowsWhenRepoIsNull() {
+
+        //Act
+        NullPointerException exception = assertThrows(NullPointerException.class, () ->
+                _service = new PublicationService(
+                        null,
+                        _publicationFactoryDouble,
+                        _iGenreRepoDouble,
+                        _iAuthorRepoDouble,
+                        _publicationResponseDTOMapper)
+        );
+
+        //Assert
+        assertEquals(repoExceptionMessage, exception.getMessage());
+    }
+
+    @Test
+    void constructorThrowsWhenFactoryIsNull() {
+
+        //Act
+        NullPointerException exception = assertThrows(NullPointerException.class, () ->
+                _service = new PublicationService(
+                        _iPublicationRepoDouble,
+                        null,
+                        _iGenreRepoDouble,
+                        _iAuthorRepoDouble,
+                        _publicationResponseDTOMapper)
+        );
+
+        //Assert
+        assertEquals(factoryExceptionMessage, exception.getMessage());
+    }
+
+    @Test
+    void constructorThrowsWhenGenreRepoIsNull() {
+
+        //Act
+        NullPointerException exception = assertThrows(NullPointerException.class, () ->
+                _service = new PublicationService(
+                        _iPublicationRepoDouble,
+                        _publicationFactoryDouble,
+                        null,
+                        _iAuthorRepoDouble,
+                        _publicationResponseDTOMapper)
+        );
+
+        //Assert
+        assertEquals(genreRepoExceptionMessage, exception.getMessage());
+    }
+
+    @Test
+    void constructorThrowsWhenAuthorRepoIsNull() {
+
+        //Act
+        NullPointerException exception = assertThrows(NullPointerException.class, () ->
+                _service = new PublicationService(
+                        _iPublicationRepoDouble,
+                        _publicationFactoryDouble,
+                        _iGenreRepoDouble,
+                        null,
+                        _publicationResponseDTOMapper)
+        );
+
+        //Assert
+        assertEquals(authorRepoExceptionMessage, exception.getMessage());
+    }
+
+    @Test
+    void constructorThrowsWhenMapperIsNull() {
+
+        //Act
+        NullPointerException exception = assertThrows(NullPointerException.class, () ->
+                _service = new PublicationService(
+                        _iPublicationRepoDouble,
+                        _publicationFactoryDouble,
+                        _iGenreRepoDouble,
+                        _iAuthorRepoDouble,
+                        null)
+        );
+
+        //Assert
+        assertEquals(mapperExceptionMessage, exception.getMessage());
     }
 
     @Test
     void registerPublicationThrowsWhenPublicationAlreadyExists() {
-
         //Arrange
         Title titleDouble = mock(Title.class);
         AuthorId authorIdDouble = mock(AuthorId.class);
         Year yearDouble = mock(Year.class);
         GenreId genreIdDouble = mock(GenreId.class);
         Publication publicationDouble = mock(Publication.class);
+        PublicationId publicationIdDouble = mock(PublicationId.class);
+        Genre genreDouble = mock(Genre.class);
+        Author authorDouble = mock(Author.class);
 
-        when(_publicationFactoryDouble.createPublication(titleDouble, authorIdDouble, yearDouble, genreIdDouble))
-                .thenReturn(publicationDouble);
-        when(_iGenreRepoDouble.containsOfIdentity(genreIdDouble))
-                .thenReturn(true);
-        when(_iPublicationRepoDouble.containsOfIdentity(any()))
+        when(_iAuthorRepoDouble.ofIdentity(authorIdDouble))
+                .thenReturn(Optional.of(authorDouble));
+
+        when(_iGenreRepoDouble.ofIdentity(genreIdDouble))
+                .thenReturn(Optional.of(genreDouble));
+
+        when(_publicationFactoryDouble.createPublication(
+                titleDouble,
+                authorIdDouble,
+                yearDouble,
+                genreIdDouble
+        )).thenReturn(publicationDouble);
+
+        when(publicationDouble.identity())
+                .thenReturn(publicationIdDouble);
+
+        when(_iPublicationRepoDouble.containsOfIdentity(publicationIdDouble))
                 .thenReturn(true);
 
         //Act
-        IllegalStateException exception = assertThrows(IllegalStateException.class, () ->
-                _service.registerPublication(titleDouble, authorIdDouble, yearDouble, genreIdDouble)
+        IllegalStateException exception = assertThrows(
+                IllegalStateException.class,
+                () -> _service.registerPublication(
+                        titleDouble,
+                        authorIdDouble,
+                        yearDouble,
+                        genreIdDouble
+                )
         );
 
         //Assert
@@ -103,9 +253,13 @@ class PublicationServiceTest {
         AuthorId authorIdDouble = mock(AuthorId.class);
         Year yearDouble = mock(Year.class);
         GenreId genreIdDouble = mock(GenreId.class);
+        Author authorDouble = mock(Author.class);
 
-        when(_iGenreRepoDouble.containsOfIdentity(genreIdDouble))
-                .thenReturn(false);
+        when(_iAuthorRepoDouble.ofIdentity(authorIdDouble))
+                .thenReturn(Optional.of(authorDouble));
+
+        when(_iGenreRepoDouble.ofIdentity(genreIdDouble))
+                .thenReturn(Optional.empty());
 
         //Act
         NoSuchElementException exception = assertThrows(NoSuchElementException.class, () ->
@@ -117,26 +271,23 @@ class PublicationServiceTest {
     }
 
     @Test
-    void constructorThrowsWhenRepoIsNull() {
+    void registerPublicationThrowsWhenAuthorIdDoesntExists() {
+
+        //Arrange
+        Title titleDouble = mock(Title.class);
+        AuthorId authorIdDouble = mock(AuthorId.class);
+        Year yearDouble = mock(Year.class);
+        GenreId genreIdDouble = mock(GenreId.class);
+
+        when(_iAuthorRepoDouble.ofIdentity(authorIdDouble))
+                .thenReturn(Optional.empty());
 
         //Act
-        NullPointerException exception = assertThrows(NullPointerException.class, () ->
-                _service = new PublicationService(null, _publicationFactoryDouble, _iGenreRepoDouble)
+        NoSuchElementException exception = assertThrows(NoSuchElementException.class, () ->
+                _service.registerPublication(titleDouble, authorIdDouble, yearDouble, genreIdDouble)
         );
 
         //Assert
-        assertEquals(repoExceptionMessage, exception.getMessage());
-    }
-
-    @Test
-    void constructorThrowsWhenFactoryIsNull() {
-
-        //Act
-        NullPointerException exception = assertThrows(NullPointerException.class, () ->
-                _service = new PublicationService(_iPublicationRepoDouble, null, _iGenreRepoDouble)
-        );
-
-        //Assert
-        assertEquals(factoryExceptionMessage, exception.getMessage());
+        assertEquals(authorIdDoesntExistExceptionMessage, exception.getMessage());
     }
 }
