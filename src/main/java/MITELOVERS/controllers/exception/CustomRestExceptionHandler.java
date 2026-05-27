859bdcf4225fbc8501795650d0144ac6556e2f22
Carolina Leite <1251987@isep.ipp.pt>
2026-05-27 14:48:59 +0100

    refactor: centralize REST exception handling with consistent HTTP status mappings #932

--- src/main/java/MITELOVERS/controllers/exception/CustomRestExceptionHandler.java
+++ src/main/java/MITELOVERS/controllers/exception/CustomRestExceptionHandler.java
@@ -6,8 +6,21 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.NoSuchElementException;
+
 @ControllerAdvice
 public class CustomRestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<Object> handleNoSuchElement(NoSuchElementException ex) {
+
+        ApiError apiError = new ApiError(
+                HttpStatus.NOT_FOUND,
+                ex.getMessage()
+        );
+
+        return new ResponseEntity<>(apiError, apiError.getStatus());
+    }
 
     @ExceptionHandler(IllegalStateException.class)
     public ResponseEntity<Object> handleIllegalState(IllegalStateException ex) {
@@ -24,6 +37,40 @@ public class CustomRestExceptionHandler extends ResponseEntityExceptionHandler {
                 HttpStatus.BAD_REQUEST,
                 ex.getMessage()
         );
-        return new ResponseEntity<> (apiError, apiError.getStatus());
+        return new ResponseEntity<>(apiError, apiError.getStatus());
     }
+
+    @ExceptionHandler(SecurityException.class)
+    public ResponseEntity<Object> handleSecurity(SecurityException ex) {
+
+        ApiError apiError = new ApiError(
+                HttpStatus.FORBIDDEN,
+                ex.getMessage()
+        );
+
+        return new ResponseEntity<>(apiError, apiError.getStatus());
+    }
+
+    @ExceptionHandler(UnsupportedOperationException.class)
+    public ResponseEntity<Object> handleUnsupportedOperation(UnsupportedOperationException ex) {
+
+        ApiError apiError = new ApiError(
+                HttpStatus.NOT_IMPLEMENTED,
+                ex.getMessage()
+        );
+
+        return new ResponseEntity<>(apiError, apiError.getStatus());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Object> handleRuntime(RuntimeException ex) {
+
+        ApiError apiError = new ApiError(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                "Unexpected server error"
+        );
+
+        return new ResponseEntity<>(apiError, apiError.getStatus());
+    }
+
 }
