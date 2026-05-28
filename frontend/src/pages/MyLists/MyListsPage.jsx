b40660b8453abef6b795da873cb09808b0e66c9a
Vitor Barros <1252009@isep.ipp.pt>
2026-05-28 14:56:18 +0100

    feat: added MyListsPage and tests, refs #955

---
+++ frontend/src/pages/MyLists/MyListsPage.jsx
@@ -0,0 +1,9 @@
+import {DefaultLayout} from "../../components/layout/DefaultLayout.tsx";
+
+export default function MyListsPage() {
+    return (
+        <DefaultLayout>
+            <h1>My Lists</h1>
+        </DefaultLayout>
+    );
+}
