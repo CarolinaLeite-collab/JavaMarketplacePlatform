25629c56a5d36e8fb9f96f040c33fafce57c6a68
Pedro <1252001@isep.ipp.pt>
2026-05-29 16:35:23 +0100

    refactor(#926): decouple CreateAuthorController migrating dependency layer to service

--- src/main/java/MITELOVERS/controllers/cli/CreateAuthorController.java
+++ src/main/java/MITELOVERS/controllers/cli/CreateAuthorController.java
@@ -1,38 +1,26 @@
 package MITELOVERS.controllers.cli;
 
-import MITELOVERS.domain.author.Author;
-import MITELOVERS.domain.author.AuthorFactory;
-import MITELOVERS.domain.repository.IAuthorRepo;
-import MITELOVERS.domain.valueobject.Name;
+import MITELOVERS.applicationservices.AuthorService;
+import MITELOVERS.dto.AuthorResponseDTO;
 import org.springframework.stereotype.Component;
 
 /**
  * Controller responsible for creating new authors in the system.
  * <p>
- * This controller delegates the creation of {@link Author} instances to the
- * {@link IAuthorRepo}, ensuring that author names are valid and not empty.
+ * This controller delegates the creation of authors to {@link AuthorService}.
  * </p>
  */
 
 @Component
 public class CreateAuthorController {
 
-    private IAuthorRepo _iAuthorRepo;
-    private AuthorFactory _authorFactory;
+    private final AuthorService _authorService;
 
-    public CreateAuthorController(IAuthorRepo iAuthorRepo,AuthorFactory authorFactory) {
-
-        _iAuthorRepo = iAuthorRepo;
-        _authorFactory = authorFactory;
-
+    public CreateAuthorController(AuthorService authorService) {
+        _authorService = authorService;
     }
 
-    public Author createAuthor (Name authorName){
-
-        Author newAuthor = _authorFactory.createAuthor(authorName);
-
-        return _iAuthorRepo.save (newAuthor);
-
+    public AuthorResponseDTO createAuthor(String authorName) {
+        return _authorService.registerAuthor(authorName);
     }
-
 }
