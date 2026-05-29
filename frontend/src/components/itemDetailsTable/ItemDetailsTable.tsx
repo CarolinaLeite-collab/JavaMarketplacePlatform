269e7bd1a64d8a2cc816f019af5c9acfba4e2b54
mcosta <1251998@isep.ipp.pt>
2026-05-30 00:23:02 +0100

    refactor: added some styles in ItemDetailsTable component, refs #963

--- frontend/src/components/itemDetailsTable/ItemDetailsTable.tsx
+++ frontend/src/components/itemDetailsTable/ItemDetailsTable.tsx
@@ -1,4 +1,4 @@
-import { Table } from '@mantine/core';
+import {Table, useComputedColorScheme} from '@mantine/core';
 
 interface ItemDetail {
     publicationType: string;
@@ -11,6 +11,7 @@ interface Props {
 }
 
 export function ItemDetailTable({ item }: Props) {
+    const colorScheme = useComputedColorScheme('light');
 
     const identifierLabel =
         item.publicationType === 'Book'     ? 'ISBN' :
@@ -18,28 +19,63 @@ export function ItemDetailTable({ item }: Props) {
         'Identifier';
 
     return (
-        <Table withTableBorder withColumnBorders>
-            <Table.Tbody>
+        <Table withTableBorder withColumnBorders
+           styles={{
+               td: {
+                   borderColor:
+                       colorScheme === 'light'
+                           ? 'var(--mantine-color-gray-5)'
+                           : 'var(--mantine-color-dark-2)',
+               },
 
+               tr: {
+                   borderColor:
+                       colorScheme === 'light'
+                           ? 'var(--mantine-color-gray-4)'
+                           : 'var(--mantine-color-dark-3)',
+               },
+           }}
+        >
+            <Table.Tbody>
                 <Table.Tr>
-                    <Table.Td fw={700} w={120}>Type</Table.Td>
+                    <Table.Td fw={700} w={150}
+                      style={{
+                          backgroundColor:
+                              colorScheme === 'light'
+                                  ? 'var(--mantine-color-gray-3)'
+                                  : 'var(--mantine-color-dark-4)',
+                      }}
+                    >Type</Table.Td>
                     <Table.Td>{item.publicationType}</Table.Td>
                 </Table.Tr>
 
                 <Table.Tr>
-                    <Table.Td fw={700}>Author</Table.Td>
+                    <Table.Td fw={700}
+                      style={{
+                          backgroundColor:
+                              colorScheme === 'light'
+                                  ? 'var(--mantine-color-gray-3)'
+                                  : 'var(--mantine-color-dark-4)',
+                      }}
+                    >Author</Table.Td>
                     <Table.Td>{item.authorName}</Table.Td>
                 </Table.Tr>
 
                 {item.identifier && (
                     <Table.Tr>
-                        <Table.Td fw={700}>
-                            {identifierLabel}
+                        <Table.Td fw={700}
+                          style={{
+                              backgroundColor:
+                                  colorScheme === 'light'
+                                      ? 'var(--mantine-color-gray-3)'
+                                      : 'var(--mantine-color-dark-4)',
+                          }}
+                        >
+                        {identifierLabel}
                         </Table.Td>
                         <Table.Td>{item.identifier}</Table.Td>
                     </Table.Tr>
                 )}
-
             </Table.Tbody>
         </Table>
     );
