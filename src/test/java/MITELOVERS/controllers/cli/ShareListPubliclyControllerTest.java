931a1dd0adb7e5410d3409544b0f02c656e05c47
rafaelar <1252002@isep.ipp.pt>
2026-05-29 16:15:37 +0100

    fix: updates imports in ListOfItems-related CLI controllers and respective tests, refs #957

--- src/test/java/MITELOVERS/controllers/cli/ShareListPubliclyControllerTest.java
+++ src/test/java/MITELOVERS/controllers/cli/ShareListPubliclyControllerTest.java
@@ -1,24 +1,14 @@
 package MITELOVERS.controllers.cli;
 
 import MITELOVERS.applicationservices.ListOfItemsService;
-import MITELOVERS.controllers.cli.ShareListPubliclyController;
-import MITELOVERS.domain.listofitems.ListOfItems;
-import MITELOVERS.domain.repository.IListOfItemsRepo;
-import MITELOVERS.domain.valueobject.ListOfItemsId;
-import MITELOVERS.domain.valueobject.SharedDuration;
-import MITELOVERS.domain.valueobject.UserId;
-import MITELOVERS.dto.ListOfItemsResponseDTO;
-import MITELOVERS.dto.MakeListPublicRequestDTO;
-import org.junit.jupiter.api.BeforeEach;
+import MITELOVERS.dto.response.ListOfItemsResponseDTO;
+import MITELOVERS.dto.request.MakeListPublicRequestDTO;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ActiveProfiles;
-
-import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
