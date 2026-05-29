9cc7b0af90a916eb29ade3bf3643442fbc9cf3e6
Carolina Leite <1251987@isep.ipp.pt>
2026-05-29 15:32:07 +0100

    feat: implement getAllPublicationTypes service method #927

--- src/main/java/MITELOVERS/applicationservices/PublicationTypeService.java
+++ src/main/java/MITELOVERS/applicationservices/PublicationTypeService.java
@@ -1,9 +1,41 @@
 package MITELOVERS.applicationservices;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import MITELOVERS.domain.publicationtype.PublicationType;
+import MITELOVERS.domain.repository.IPublicationTypeRepo;
 
-@Getter
-@AllArgsConstructor
+import MITELOVERS.dto.response.PublicationTypeResponseDTO;
+import MITELOVERS.mapper.PublicationTypeResponseDTOMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class PublicationTypeService {
+
+    private final IPublicationTypeRepo _iPublicationTypeRepo;
+
+    private final PublicationTypeResponseDTOMapper _mapper;
+
+    public PublicationTypeService(IPublicationTypeRepo iPublicationTypeRepo, PublicationTypeResponseDTOMapper mapper){
+
+        _iPublicationTypeRepo = iPublicationTypeRepo;
+        _mapper = mapper;
+
+    }
+
+    public List<PublicationTypeResponseDTO> getAllPublicationTypes(){
+
+        Iterable<PublicationType> publicationTypes = _iPublicationTypeRepo.findAll();
+
+        List<PublicationTypeResponseDTO> response = new ArrayList<>();
+
+        for (PublicationType publicationType : publicationTypes) {
+
+            response.add (_mapper.toResponseDTO(publicationType));
+        }
+
+        return response;
+
+    }
 }
