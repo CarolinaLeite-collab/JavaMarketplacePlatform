d78c5814b5dbae3b718fe57d1b39874b578874ff
mcosta <1251998@isep.ipp.pt>
2026-05-30 00:25:34 +0100

    refactor: added ItemAccordion in MyLibraryPage, refs #970

--- frontend/src/pages/MyLibrary/MyLibraryPage.jsx
+++ frontend/src/pages/MyLibrary/MyLibraryPage.jsx
@@ -1,19 +1,28 @@
 import {DefaultLayout} from "../../components/layout/DefaultLayout.tsx";
-import {ItemDetailTable} from "../../components/itemDetailsTable/ItemDetailsTable.js";
+import {ItemAccordion} from "@/components/accordion/ItemAccordion.js";
 
 export default function MyLibraryPage() {
-
-
     return (
         <DefaultLayout title="My Library" subtitle="CHECK OUT YOUR ITEMS:">
-            <p>Page content goes here.</p>
-            <ItemDetailTable
-                item={{
+            <ItemAccordion
+                items={[
+                    {
+                    itemId: 'ITEM-001',
+                    title: 'The War of the Worlds',
+                    imageUrl: null,
                     publicationType: 'Book',
                     authorName: 'H.G. Wells',
                     identifier: '9781784872113'
-                }}
-            />
+                },
+                {
+                    itemId: 'ITM-002',
+                    title: 'Duna',
+                    imageUrl: null,
+                    publicationType: 'Book',
+                    authorName: 'Frank Herbert',
+                    identifier: '9780593099322'
+                }]
+                }/>
         </DefaultLayout>
     );
 }
