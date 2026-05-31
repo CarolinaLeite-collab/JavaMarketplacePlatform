92d2ca029fc9247424af038d6e70681e2dab4350
edu_link <1251996@isep.ipp.pt>
2026-05-31 18:26:56 +0100

    task: Removed unnecessary imports #939

--- src/main/java/MITELOVERS/controllers/cli/GetDirectSaleItemsByGenreController.java
+++ src/main/java/MITELOVERS/controllers/cli/GetDirectSaleItemsByGenreController.java
@@ -1,21 +1,9 @@
 package MITELOVERS.controllers.cli;
 
 import MITELOVERS.applicationservices.DirectSaleService;
-import MITELOVERS.domain.directsale.DirectSale;
-import MITELOVERS.domain.edition.Edition;
-import MITELOVERS.domain.item.Item;
-import MITELOVERS.domain.publication.Publication;
-import MITELOVERS.domain.repository.IDirectSaleRepo;
-import MITELOVERS.domain.repository.IEditionRepo;
-import MITELOVERS.domain.repository.IItemRepo;
-import MITELOVERS.domain.repository.IPublicationRepo;
-import MITELOVERS.domain.valueobject.GenreId;
-import MITELOVERS.domain.valueobject.ItemId;
 import MITELOVERS.dto.response.DSFilteredItemsResponseDTO;
 import org.springframework.stereotype.Controller;
 
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Controller responsible for retrieving items available in direct sales filtered by genre.
--- src/test/java/MITELOVERS/controllers/cli/GetDirectSaleItemsByGenreControllerTest.java
+++ src/test/java/MITELOVERS/controllers/cli/GetDirectSaleItemsByGenreControllerTest.java
@@ -1,15 +1,6 @@
 package MITELOVERS.controllers.cli;
 
 import MITELOVERS.applicationservices.DirectSaleService;
-import MITELOVERS.controllers.cli.GetDirectSaleItemsByGenreController;
-import MITELOVERS.domain.directsale.DirectSale;
-import MITELOVERS.domain.edition.Edition;
-import MITELOVERS.domain.item.Item;
-import MITELOVERS.domain.publication.Publication;
-import MITELOVERS.domain.repository.IDirectSaleRepo;
-import MITELOVERS.domain.repository.IEditionRepo;
-import MITELOVERS.domain.repository.IItemRepo;
-import MITELOVERS.domain.repository.IPublicationRepo;
 import MITELOVERS.domain.valueobject.*;
 import MITELOVERS.dto.response.DSFilteredItemsResponseDTO;
 import org.junit.jupiter.api.Test;
@@ -20,11 +11,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
