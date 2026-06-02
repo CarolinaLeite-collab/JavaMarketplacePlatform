df68482a8c6f869a6b1398819a90fc6c99b1e189
gui1251991 <1251991@isep.ipp.pt>
2026-06-03 00:05:23 +0100

    refactor: update AppRoutes with marketplace #955

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import MySalesPage from '../pages/MySales/MySalesPage.jsx';
 import MyListsPage from '../pages/MyLists/MyListsPage.jsx';
 import MyLibraryPage from '../pages/MyLibrary/MyLibraryPage.jsx';
+import Marketplace from "@/pages/Marketplace/Marketplace.jsx";
 
 export function AppRoutes() {
     return (
@@ -9,6 +10,7 @@ export function AppRoutes() {
             <Route path="/" element={<MySalesPage />} />
             <Route path="/my-lists" element={<MyListsPage />} />
             <Route path="/my-library" element={<MyLibraryPage />} />
+            <Route path="/marketplace" element={<Marketplace />} />
         </Routes>
     );
 }
