4b60da74968a6f036b74dd7b714aacdd8f090110
mcosta <1251998@isep.ipp.pt>
2026-06-02 23:40:00 +0100

    feat: register LibraryProvider in App root, closes #995

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,18 @@ import { MantineProvider } from '@mantine/core';
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './routes/AppRoutes';
 import '@mantine/core/styles.css';
+import {AppProvider, LibraryProvider} from "@/context/AppProvider.jsx";
 
 export default function App() {
   return (
       <MantineProvider defaultColorScheme="auto">
-        <BrowserRouter>
-              <AppRoutes />
-        </BrowserRouter>
+          <BrowserRouter>
+              <AppProvider>
+                  <LibraryProvider>
+                      <AppRoutes />
+                  </LibraryProvider>
+              </AppProvider>
+          </BrowserRouter>
       </MantineProvider>
   );
 }
