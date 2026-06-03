3921d5e045664e66bda6ab643369a6f233416788
Vivian Sandler <1252010@isep.ipp.pt>
2026-06-03 16:12:49 +0100

    refactor: connect CreateSaleModal to sales state, actions, and API, ref. #1013, #975

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import { MantineProvider } from '@mantine/core';
+import { Notifications } from '@mantine/notifications';
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './routes/AppRoutes';
 import '@mantine/core/styles.css';
+import '@mantine/notifications/styles.css';
 import {AppProvider, LibraryProvider} from "@/context/AppProvider.jsx";
 
 export default function App() {
   return (
       <MantineProvider defaultColorScheme="auto">
+          <Notifications />
           <BrowserRouter>
               <AppProvider>
                   <LibraryProvider>
