1756f9a66c5dadef67199e3a8c1bf39f2dfef435
Pedro <1252001@isep.ipp.pt>
2026-06-03 15:16:25 +0100

    feat: add OPTIONS endpoint with HATEOAS links to AuthorRestController #1009

--- src/main/java/MITELOVERS/controllers/rest/AuthorRestController.java
+++ src/main/java/MITELOVERS/controllers/rest/AuthorRestController.java
@@ -1,74 +1,96 @@
 package MITELOVERS.controllers.rest;
 
 import MITELOVERS.applicationservices.AuthorService;
+import MITELOVERS.applicationservices.UserService;
+import MITELOVERS.controllers.linkprovider.AuthorLinkProvider;
+import MITELOVERS.domain.author.Author;
+import MITELOVERS.domain.user.User;
 import MITELOVERS.dto.response.AuthorResponseDTO;
 import MITELOVERS.dto.request.AuthorRequestDTO;
+import MITELOVERS.mapper.AuthorResponseDTOMapper;
+import org.springframework.hateoas.RepresentationModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
-
-/**
- * REST controller responsible for exposing author-related endpoints via HTTP endpoints.
- */
 
 @RestController
 @RequestMapping("/authors")
 public class AuthorRestController {
 
     private final AuthorService _authorService;
+    private final AuthorLinkProvider _authorLinkProvider;
+    private final UserService _userService;
+    private final AuthorResponseDTOMapper _authorResponseDTOMapper;
 
-    public AuthorRestController(AuthorService authorService) {
+    public AuthorRestController(AuthorService authorService,
+                                AuthorLinkProvider authorLinkProvider,
+                                UserService userService,
+                                AuthorResponseDTOMapper authorResponseDTOMapper) {
         _authorService = authorService;
+        _authorLinkProvider = authorLinkProvider;
+        _userService = userService;
+        _authorResponseDTOMapper = authorResponseDTOMapper;
+    }
+
+    @RequestMapping(method = RequestMethod.OPTIONS)
+    public ResponseEntity<RepresentationModel<?>> options(@RequestParam("email") String email) {
+        User user = _userService.getUserByEmail(email);
+        RepresentationModel<?> model = new RepresentationModel<>();
+        _authorLinkProvider.getLinks(user).forEach(model::add);
+        return ResponseEntity.ok(model);
     }
 
     @PostMapping
     public ResponseEntity<AuthorResponseDTO> registerAuthorAndReturnDTO(
             @RequestBody AuthorRequestDTO info) {
 
-        AuthorResponseDTO authorResponseDTO = _authorService.registerAuthor(
-                info.getAuthorName()
-        );
+        // 1. Service handles domain logic and returns domain object
+        Author savedAuthor = _authorService.registerAuthor(info.getAuthorName());
 
-        authorResponseDTO.add(
-                linkTo(
-                        methodOn(AuthorRestController.class)
-                                .getAuthorById(authorResponseDTO.getAuthorId())
-                ).withSelfRel()
-        );
+        // 2. Controller converts domain object to DTO
+        AuthorResponseDTO responseDTO = _authorResponseDTOMapper.toModel(savedAuthor);
 
-        return new ResponseEntity<>(authorResponseDTO, HttpStatus.CREATED);
+        // 3. Controller adds HATEOAS link
+        responseDTO.add(linkTo(methodOn(AuthorRestController.class)
+                .getAuthorById(responseDTO.getAuthorId())).withSelfRel());
+
+        return new ResponseEntity<>(responseDTO, HttpStatus.CREATED);
     }
 
     @GetMapping
     public ResponseEntity<List<AuthorResponseDTO>> getAllAuthors() {
+        Iterable<Author> authors = _authorService.getAllAuthors();
 
-        List<AuthorResponseDTO> authors = _authorService.getAllAuthors();
+        List<AuthorResponseDTO> dtoList = new ArrayList<>();
+        for (Author author : authors) {
+            AuthorResponseDTO dto = _authorResponseDTOMapper.toModel(author);
+            dto.add(linkTo(methodOn(AuthorRestController.class)
+                    .getAuthorById(dto.getAuthorId())).withSelfRel());
+            dtoList.add(dto);
+        }
 
-        if (authors.isEmpty()) {
+        if (dtoList.isEmpty()) {
             return ResponseEntity.noContent().build();
         }
 
-        for (AuthorResponseDTO author : authors) {
-            author.add(
-                    linkTo(
-                            methodOn(AuthorRestController.class)
-                                    .getAuthorById(author.getAuthorId())
-                    ).withSelfRel()
-            );
-        }
-
-        return ResponseEntity.ok(authors);
+        return ResponseEntity.ok(dtoList);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<AuthorResponseDTO> getAuthorById(
-            @PathVariable String id) {
+    public ResponseEntity<AuthorResponseDTO> getAuthorById(@PathVariable String id) {
+        Author author = _authorService.getAuthorById(id)
+                .orElseThrow(() -> new java.util.NoSuchElementException("Author not found"));
 
-        return ResponseEntity.ok().build();
+        AuthorResponseDTO dto = _authorResponseDTOMapper.toModel(author);
+        dto.add(linkTo(methodOn(AuthorRestController.class)
+                .getAuthorById(dto.getAuthorId())).withSelfRel());
+
+        return ResponseEntity.ok(dto);
     }
 }
