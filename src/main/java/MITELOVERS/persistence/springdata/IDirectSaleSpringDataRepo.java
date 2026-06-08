328fba805007fcc20edc79cf7a9e8013ab26146b
edu_link <1251996@isep.ipp.pt>
2026-06-08 08:40:05 +0100

    task: Added DELETE and findExpired methods to persistence #1036

--- src/main/java/MITELOVERS/persistence/springdata/IDirectSaleSpringDataRepo.java
+++ src/main/java/MITELOVERS/persistence/springdata/IDirectSaleSpringDataRepo.java
@@ -2,6 +2,7 @@ package MITELOVERS.persistence.springdata;
 
 import MITELOVERS.persistence.jpa.datamodel.DirectSaleDataModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -10,5 +11,13 @@ public interface IDirectSaleSpringDataRepo extends JpaRepository<DirectSaleDataM
     List<DirectSaleDataModel> findByItemsIdOrderByCreationDateAsc(List<String> itemsId);
 
     List<DirectSaleDataModel> findByItemsIdOrderByCreationDateDesc(List<String> itemsId);
+
+    @Query("""
+    SELECT ds
+    FROM DirectSaleDataModel ds
+    WHERE ds.timeLimit IS NOT NULL
+      AND FUNCTION('DATEADD', 'SECOND', ds.timeLimit, ds.creationDate) < CURRENT_TIMESTAMP
+""")
+    List<DirectSaleDataModel> findExpiredRaw();
 
 }
