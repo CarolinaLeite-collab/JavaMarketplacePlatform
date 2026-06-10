bcff9c7982afabd68db3b33b24c07a743a35786b
Vitor Barros <1252009@isep.ipp.pt>
2026-06-10 15:26:44 +0100

    feat: created DirectSaleStatus enum value object class, refs #1047

---
+++ src/main/java/MITELOVERS/domain/valueobject/DirectSaleStatus.java
@@ -0,0 +1,12 @@
+package MITELOVERS.domain.valueobject;
+
+import MITELOVERS.ddd.ValueObject;
+
+public enum DirectSaleStatus implements ValueObject {
+
+    ACTIVE,
+    COMPLETED,
+    CANCELLED,
+    EXPIRED
+
+}
