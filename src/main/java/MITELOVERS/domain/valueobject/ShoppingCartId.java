4c98633d0be7b315e2247ebe90aa407be36269f1
Vitor Barros <1252009@isep.ipp.pt>
2026-06-12 12:01:33 +0100

    feat: added ShoppingCartId class + tests, refs #1055

---
+++ src/main/java/MITELOVERS/domain/valueobject/ShoppingCartId.java
@@ -0,0 +1,44 @@
+package MITELOVERS.domain.valueobject;
+
+import MITELOVERS.ddd.DomainId;
+
+import java.util.UUID;
+
+public class ShoppingCartId implements DomainId {
+
+    private String _id;
+
+    public ShoppingCartId() {
+
+        _id = "SC-" + UUID.randomUUID().toString().substring(0, 8).toUpperCase();
+
+    }
+
+    public ShoppingCartId(String id) {
+
+        if (id == null || !id.matches("SC-[A-Z0-9]{8}")) {
+            throw new IllegalArgumentException("Invalid ShoppingCartId format!");
+        }
+
+        _id = id;
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ShoppingCartId that = (ShoppingCartId) o;
+        return _id.equals(that._id);
+    }
+
+    @Override
+    public int hashCode() {
+        return _id.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return _id;
+    }
+}
