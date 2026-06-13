03020dda48183449a0d81b389ba0c05100da45f4
Vitor Barros <1252009@isep.ipp.pt>
2026-06-13 23:18:50 +0100

    feat: defined IShoppingCartSpringDataRepo interface, refs #1101

---
+++ src/main/java/MITELOVERS/persistence/springdata/IShoppingCartSpringDataRepo.java
@@ -0,0 +1,12 @@
+package MITELOVERS.persistence.springdata;
+
+import MITELOVERS.persistence.jpa.datamodel.ShoppingCartDataModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface IShoppingCartSpringDataRepo extends JpaRepository<ShoppingCartDataModel, String> {
+
+    Optional<ShoppingCartDataModel> findByBuyerId(String buyerId);
+
+}
