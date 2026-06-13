4df457c72ff4f9fee558036ca59a98e37c3f7677
Vivian Sandler <1252010@isep.ipp.pt>
2026-06-13 01:08:26 +0100

    feat: create PlaceBidRequestDTO and BidResponseDTO, ref. #1083

---
+++ src/main/java/MITELOVERS/dto/response/BidResponseDTO.java
@@ -0,0 +1,26 @@
+package MITELOVERS.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+/**
+ * Data Transfer Object used to expose bid information in API responses
+ */
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BidResponseDTO extends RepresentationModel<BidResponseDTO> {
+
+    private String bidId;
+    private String auctionId;
+    private String buyerId;
+    private BigDecimal offerPrice;
+    private String currency;
+    private Instant bidDate;
+}
