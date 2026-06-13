0e07a34f0ac60d6c4f683a6231a07d76080b1b18
Vitor Barros <1252009@isep.ipp.pt>
2026-06-13 17:14:40 +0100

    feat: updated ShoppingCartLine class + tests, refs #1056

--- src/main/java/MITELOVERS/domain/shoppingcart/ShoppingCartLine.java
+++ src/main/java/MITELOVERS/domain/shoppingcart/ShoppingCartLine.java
@@ -1,17 +1,84 @@
 package MITELOVERS.domain.shoppingcart;
 
 import MITELOVERS.ddd.DomainEntity;
-import MITELOVERS.domain.valueobject.ShoppingCartLineId;
+import MITELOVERS.domain.valueobject.*;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class ShoppingCartLine implements DomainEntity<ShoppingCartLineId> {
 
+    private ShoppingCartLineId _shoppingCartLineId;
+    private DirectSaleId _directSaleId;
+    private ItemId _itemId;
+    private UserId _sellerId;
+    private Price _priceAtAddition;
+    private LocalDateTime _addedAt;
+
+    public ShoppingCartLine(DirectSaleId directSaleId,
+                            ItemId itemId,
+                            UserId sellerId,
+                            Price priceAtAddition) {
+
+        _shoppingCartLineId = new ShoppingCartLineId();
+        _directSaleId = Objects.requireNonNull(directSaleId, "directSaleId cannot be null!");
+        _itemId = Objects.requireNonNull(itemId, "itemId cannot be null!");
+        _sellerId = Objects.requireNonNull(sellerId, "sellerId cannot be null!");
+        _priceAtAddition = Objects.requireNonNull(priceAtAddition, "priceAtAddition cannot be null!");
+        _addedAt = LocalDateTime.now();
+
+    }
+
+    //Re-hydration
+    public ShoppingCartLine(ShoppingCartLineId shoppingCartLineId,
+                            DirectSaleId directSaleId,
+                            ItemId itemId,
+                            UserId sellerId,
+                            Price priceAtAddition,
+                            LocalDateTime addetAt) {
+
+        this(directSaleId, itemId, sellerId, priceAtAddition);
+        _shoppingCartLineId = Objects.requireNonNull(shoppingCartLineId, "ShoppingCartLineId cannot be null!");
+        _addedAt = Objects.requireNonNull(addetAt, "Time of addition cannot be null!");
+
+    }
+
+    public DirectSaleId getDirectSaleId() {
+        return _directSaleId;
+    }
+    public ItemId getItemId() {
+        return _itemId;
+    }
+    public UserId getSellerId() {
+        return _sellerId;
+    }
+    public Price getPriceAtAddition() {
+        return _priceAtAddition;
+    }
+    public LocalDateTime getAddedAt() {
+        return _addedAt;
+    }
+
     @Override
     public ShoppingCartLineId identity() {
-        return null;
+        return _shoppingCartLineId;
     }
 
     @Override
     public boolean sameAs(Object object) {
-        return false;
+        return equals(object);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ShoppingCartLine that = (ShoppingCartLine) o;
+        return Objects.equals(_shoppingCartLineId, that._shoppingCartLineId) && Objects.equals(_directSaleId, that._directSaleId) && Objects.equals(_itemId, that._itemId) && Objects.equals(_sellerId, that._sellerId) && Objects.equals(_priceAtAddition, that._priceAtAddition) && Objects.equals(_addedAt, that._addedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(_shoppingCartLineId, _directSaleId, _itemId, _sellerId, _priceAtAddition, _addedAt);
+    }
+
 }
---
+++ src/test/java/MITELOVERS/domain/shoppingcart/ShoppingCartLineTest.java
@@ -0,0 +1,325 @@
+package MITELOVERS.domain.shoppingcart;
+
+import MITELOVERS.domain.valueobject.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+@ExtendWith(MockitoExtension.class)
+class ShoppingCartLineTest {
+
+    @Mock
+    private ShoppingCartLineId _shoppingCartLineIdDouble;
+
+    @Mock
+    private DirectSaleId _directSaleIdDouble;
+
+    @Mock
+    private ItemId _itemIdDouble;
+
+    @Mock
+    private UserId _sellerIdDouble;
+
+    @Mock
+    private Price _priceAtAdditionDouble;
+
+    private LocalDateTime _localDateTime;
+
+    @BeforeEach
+    void setUp() {
+
+        _localDateTime = LocalDateTime.now();
+
+    }
+
+    @Test
+    void testFourArgumentConstructor() {
+
+        //SUT
+        new ShoppingCartLine(_directSaleIdDouble, _itemIdDouble, _sellerIdDouble, _priceAtAdditionDouble);
+
+    }
+
+    @Test
+    void testSixArgumentConstructor() {
+
+        //SUT
+        new ShoppingCartLine(_shoppingCartLineIdDouble, _directSaleIdDouble, _itemIdDouble, _sellerIdDouble, _priceAtAdditionDouble, _localDateTime);
+
+    }
+
+    @Test
+    void testConstructorShouldThrowIfDirectSaleIdIsNull() {
+
+        //SUT + Assert
+        assertThrows(NullPointerException.class,
+                () -> new ShoppingCartLine(null, _itemIdDouble, _sellerIdDouble, _priceAtAdditionDouble)
+        );
+
+    }
+
+    @Test
+    void testConstructorShouldThrowIfItemIdIsNull() {
+
+        //SUT + Assert
+        assertThrows(NullPointerException.class,
+                () -> new ShoppingCartLine(_directSaleIdDouble, null, _sellerIdDouble, _priceAtAdditionDouble)
+        );
+
+    }
+
+    @Test
+    void testConstructorShouldThrowIfUserIdIsNull() {
+
+        //SUT + Assert
+        assertThrows(NullPointerException.class,
+                () -> new ShoppingCartLine(_directSaleIdDouble, _itemIdDouble, null, _priceAtAdditionDouble)
+        );
+
+    }
+
+    @Test
+    void testConstructorShouldThrowIfPriceAtAdditionIsNull() {
+
+        //SUT + Assert
+        assertThrows(NullPointerException.class,
+                () -> new ShoppingCartLine(_directSaleIdDouble, _itemIdDouble, _sellerIdDouble, null)
+        );
+
+    }
+
+    @Test
+    void testConstructorShouldThrowIfShoppingCartIdIsNull() {
+
+        //SUT + Assert
+        assertThrows(NullPointerException.class,
+                () -> new ShoppingCartLine(null, _directSaleIdDouble, _itemIdDouble, _sellerIdDouble, _priceAtAdditionDouble, _localDateTime)
+
+        );
+
+    }
+
+    @Test
+    void testConstructorShouldThrowIfTimeOfAdditionIsNull() {
+
+        //SUT + Assert
+        assertThrows(NullPointerException.class,
+                () -> new ShoppingCartLine(_shoppingCartLineIdDouble, _directSaleIdDouble, _itemIdDouble, _sellerIdDouble, _priceAtAdditionDouble, null)
+
+        );
+
+    }
+
+    @Test
+    void testGetDirectSaleId() {
+
+        //SUT
+        ShoppingCartLine cartLine = new ShoppingCartLine(_shoppingCartLineIdDouble, _directSaleIdDouble, _itemIdDouble, _sellerIdDouble, _priceAtAdditionDouble, _localDateTime);
+
+        //Act
+        DirectSaleId result = cartLine.getDirectSaleId();
+
+        //Assert
+        assertEquals(result, _directSaleIdDouble);
+
+    }
+
+    @Test
+    void testGetItemId() {
+
+        //SUT
+        ShoppingCartLine cartLine = new ShoppingCartLine(_shoppingCartLineIdDouble, _directSaleIdDouble, _itemIdDouble, _sellerIdDouble, _priceAtAdditionDouble, _localDateTime);
+
+        //Act
+        ItemId result = cartLine.getItemId();
+
+        //Assert
+        assertEquals(result, _itemIdDouble);
+
+    }
+
+    @Test
+    void testGetUserId() {
+
+        //SUT
+        ShoppingCartLine cartLine = new ShoppingCartLine(_shoppingCartLineIdDouble, _directSaleIdDouble, _itemIdDouble, _sellerIdDouble, _priceAtAdditionDouble, _localDateTime);
+
+        //Act
+        UserId result = cartLine.getSellerId();
+
+        //Assert
+        assertEquals(result, _sellerIdDouble);
+
+    }
+
+    @Test
+    void testGetPriceAtAddition() {
+
+        //SUT
+        ShoppingCartLine cartLine = new ShoppingCartLine(_shoppingCartLineIdDouble, _directSaleIdDouble, _itemIdDouble, _sellerIdDouble, _priceAtAdditionDouble, _localDateTime);
+
+        //Act
+        Price result = cartLine.getPriceAtAddition();
+
+        //Assert
+        assertEquals(result, _priceAtAdditionDouble);
+
+    }
+
+    @Test
+    void testGetAddedAt() {
+
+        //SUT
+        ShoppingCartLine cartLine = new ShoppingCartLine(_shoppingCartLineIdDouble, _directSaleIdDouble, _itemIdDouble, _sellerIdDouble, _priceAtAdditionDouble, _localDateTime);
+
+        //Act
+        LocalDateTime result = cartLine.getAddedAt();
+
+        //Assert
+        assertEquals(result, _localDateTime);
+
+    }
+
+    @Test
+    void testIdentity() {
+
+        //SUT
+        ShoppingCartLine cartLine = new ShoppingCartLine(_shoppingCartLineIdDouble, _directSaleIdDouble, _itemIdDouble, _sellerIdDouble, _priceAtAdditionDouble, _localDateTime);
+
+        //Act
+        ShoppingCartLineId result = cartLine.identity();
+
+        //Assert
+        assertEquals(result, _shoppingCartLineIdDouble);
+
+    }
+
+    @Test
+    void testSameAsShouldReturnTrueWhenObjectIsTheSame() {
+
+        //SUT
+        ShoppingCartLine cartLine1 = new ShoppingCartLine(_shoppingCartLineIdDouble, _directSaleIdDouble, _itemIdDouble, _sellerIdDouble, _priceAtAdditionDouble, _localDateTime);
+        ShoppingCartLine cartLine2 = new ShoppingCartLine(_shoppingCartLineIdDouble, _directSaleIdDouble, _itemIdDouble, _sellerIdDouble, _priceAtAdditionDouble, _localDateTime);
+
+        //Act + Assert
+        assertTrue(cartLine1.sameAs(cartLine2));
+
+    }
+
+    @Test
+    void testSameAsShouldReturnFalseWhenObjectIsNotTheSame() {
+
+        //Arrange
+        DirectSaleId directSaleId2Double = mock(DirectSaleId.class);
+
+        //SUT
+        ShoppingCartLine cartLine1 = new ShoppingCartLine(_shoppingCartLineIdDouble, _directSaleIdDouble, _itemIdDouble, _sellerIdDouble, _priceAtAdditionDouble, _localDateTime);
+        ShoppingCartLine cartLine2 = new ShoppingCartLine(_shoppingCartLineIdDouble, directSaleId2Double, _itemIdDouble, _sellerIdDouble, _priceAtAdditionDouble, _localDateTime);
+
+        //Act + Assert
+        assertFalse(cartLine1.sameAs(cartLine2));
+
+    }
+
+    @Test
+    void testEqualsShouldReturnFalseWhenObjectIsNull() {
+
+        //SUT
+        ShoppingCartLine cartLine1 = new ShoppingCartLine(_shoppingCartLineIdDouble, _directSaleIdDouble, _itemIdDouble, _sellerIdDouble, _priceAtAdditionDouble, _localDateTime);
+
+        //Act + Assert
+        assertFalse(cartLine1.equals(null));
+
+    }
+
+    @Test
+    void testEqualsShouldReturnFalseWhenObjectIsOfADifferentClass() {
+
+        //Arrange
+        String string = "test";
+
+        //SUT
+        ShoppingCartLine cartLine1 = new ShoppingCartLine(_shoppingCartLineIdDouble, _directSaleIdDouble, _itemIdDouble, _sellerIdDouble, _priceAtAdditionDouble, _localDateTime);
+
+        //Act + Assert
+        assertFalse(cartLine1.equals(string));
+
+    }
+
+    @Test
+    void testEqualsShouldReturnTrueWhenObjectIsTheSameObject() {
+
+        //SUT
+        ShoppingCartLine cartLine1 = new ShoppingCartLine(_shoppingCartLineIdDouble, _directSaleIdDouble, _itemIdDouble, _sellerIdDouble, _priceAtAdditionDouble, _localDateTime);
+
+        //Act + Assert
+        assertTrue(cartLine1.equals(cartLine1));
+
+    }
+
+    @Test
+    void testEqualsShouldReturnTrueWhenObjectIsEquals() {
+
+        //SUT
+        ShoppingCartLine cartLine1 = new ShoppingCartLine(_shoppingCartLineIdDouble, _directSaleIdDouble, _itemIdDouble, _sellerIdDouble, _priceAtAdditionDouble, _localDateTime);
+        ShoppingCartLine cartLine2 = new ShoppingCartLine(_shoppingCartLineIdDouble, _directSaleIdDouble, _itemIdDouble, _sellerIdDouble, _priceAtAdditionDouble, _localDateTime);
+
+        //Act + Assert
+        assertTrue(cartLine1.equals(cartLine2));
+
+    }
+
+    @Test
+    void testEqualsShouldReturnFalseWhenObjectIsNotTheSame() {
+
+        //Arrange
+        DirectSaleId directSaleId2Double = mock(DirectSaleId.class);
+
+        //SUT
+        ShoppingCartLine cartLine1 = new ShoppingCartLine(_shoppingCartLineIdDouble, _directSaleIdDouble, _itemIdDouble, _sellerIdDouble, _priceAtAdditionDouble, _localDateTime);
+        ShoppingCartLine cartLine2 = new ShoppingCartLine(_shoppingCartLineIdDouble, directSaleId2Double, _itemIdDouble, _sellerIdDouble, _priceAtAdditionDouble, _localDateTime);
+
+        //Act + Assert
+        assertFalse(cartLine1.equals(cartLine2));
+
+    }
+
+    @Test
+    void testHashCodeShouldBeSameWhenObjectsAreEquals() {
+
+        //SUT
+        ShoppingCartLine cartLine1 = new ShoppingCartLine(_shoppingCartLineIdDouble, _directSaleIdDouble, _itemIdDouble, _sellerIdDouble, _priceAtAdditionDouble, _localDateTime);
+        ShoppingCartLine cartLine2 = new ShoppingCartLine(_shoppingCartLineIdDouble, _directSaleIdDouble, _itemIdDouble, _sellerIdDouble, _priceAtAdditionDouble, _localDateTime);
+
+        //Act + Assert
+        assertEquals(cartLine1.hashCode(), cartLine2.hashCode());
+
+    }
+
+    @Test
+    void testHashCodeShouldBeDifferentWhenObjectsAreNotEquals() {
+
+        //Arrange
+        DirectSaleId directSaleId2Double = mock(DirectSaleId.class);
+
+        //SUT
+        ShoppingCartLine cartLine1 = new ShoppingCartLine(_shoppingCartLineIdDouble, _directSaleIdDouble, _itemIdDouble, _sellerIdDouble, _priceAtAdditionDouble, _localDateTime);
+        ShoppingCartLine cartLine2 = new ShoppingCartLine(_shoppingCartLineIdDouble, directSaleId2Double, _itemIdDouble, _sellerIdDouble, _priceAtAdditionDouble, _localDateTime);
+
+        //Act + Assert
+        assertNotEquals(cartLine1.hashCode(), cartLine2.hashCode());
+
+    }
+
+
+
+
+
+}
