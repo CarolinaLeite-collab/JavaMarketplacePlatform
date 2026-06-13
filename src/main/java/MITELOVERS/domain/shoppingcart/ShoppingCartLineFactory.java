ff92e4b4c2439b79c80fcb84610867b761490523
Vitor Barros <1252009@isep.ipp.pt>
2026-06-13 17:15:33 +0100

    feat: updated ShoppingCartLineFactory class + tests, refs #1061

--- src/main/java/MITELOVERS/domain/shoppingcart/ShoppingCartLineFactory.java
+++ src/main/java/MITELOVERS/domain/shoppingcart/ShoppingCartLineFactory.java
@@ -1,4 +1,29 @@
 package MITELOVERS.domain.shoppingcart;
 
+import MITELOVERS.domain.valueobject.*;
+
+import java.time.LocalDateTime;
+
 public class ShoppingCartLineFactory {
+
+    public ShoppingCartLine createNewShoppingCartLine(DirectSaleId directSaleId,
+                                                      ItemId itemId,
+                                                      UserId sellerId,
+                                                      Price priceAtAddition) {
+
+        return new ShoppingCartLine(directSaleId,itemId,sellerId,priceAtAddition);
+
+    }
+
+    public ShoppingCartLine createNewShoppingCartLine(ShoppingCartLineId shoppingCartLineId,
+                                                      DirectSaleId directSaleId,
+                                                      ItemId itemId,
+                                                      UserId sellerId,
+                                                      Price priceAtAddition,
+                                                      LocalDateTime addetAt) {
+
+        return new ShoppingCartLine(shoppingCartLineId, directSaleId, itemId, sellerId, priceAtAddition, addetAt);
+
+    }
+
 }
