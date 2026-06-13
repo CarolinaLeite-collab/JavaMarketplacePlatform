da75e0902d7627bc6ff37e1553218da402bc83fd
Vitor Barros <1252009@isep.ipp.pt>
2026-06-13 23:17:35 +0100

    feat: added IShoppingCartRepo interface, refs #1059

---
+++ src/main/java/MITELOVERS/domain/repository/IShoppingCartRepo.java
@@ -0,0 +1,14 @@
+package MITELOVERS.domain.repository;
+
+import MITELOVERS.ddd.IRepository;
+import MITELOVERS.domain.shoppingcart.ShoppingCart;
+import MITELOVERS.domain.valueobject.ShoppingCartId;
+import MITELOVERS.domain.valueobject.UserId;
+
+import java.util.Optional;
+
+public interface IShoppingCartRepo extends IRepository<ShoppingCartId, ShoppingCart> {
+
+    Optional<ShoppingCart> findShoppingCartByUserId(UserId userId);
+
+}
