1a76b07b5225c451b0821b140191fa21c6df8e4a
Marcelo Rocha <1251995@isep.ipp.pt>
2026-06-15 00:06:57 +0100

    feat: added /auctions/:auctionId route to AppRoutes with unit test - refs #1106

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -6,6 +6,7 @@ import ListDetailPage from '../pages/ListDetail/ListDetailPage.tsx';
 import {useContext} from 'react';
 import AppContext from '../context/AppContext';
 import {useUser} from '../context/UserContext';
+import AuctionDetailPage from '../pages/AuctionDetail/AuctionDetailPage.jsx';
 
 function ProtectedRoute({ href, children }) {
     if (!href) return <Navigate to="/" replace />;
@@ -36,6 +37,8 @@ export function AppRoutes() {
                     <ListDetailPage />
                 </ProtectedRoute>
             } />
+            <Route path="/auctions/:auctionId" element={<AuctionDetailPage />} />
+
         </Routes>
     );
 }
