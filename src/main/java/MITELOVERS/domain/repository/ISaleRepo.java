f6e8909b22d3e14db45fa4f3e1de1b96676a820a
Carolina Leite <1251987@isep.ipp.pt>
2026-06-15 09:57:41 +0100

    refactor(sale): convert repository class to interface (#1071)

--- src/main/java/MITELOVERS/domain/repository/ISaleRepo.java
+++ src/main/java/MITELOVERS/domain/repository/ISaleRepo.java
@@ -1,13 +1,11 @@
 package MITELOVERS.domain.repository;
 
-import MITELOVERS.ddd.IRepository;
 import MITELOVERS.domain.sale.Sale;
-import MITELOVERS.domain.valueobject.SaleId;
 
 /**
  * Repository interface for managing persistence and retrieval of {@link Sale} aggregates.
  */
 
-public class ISaleRepo {
+public interface ISaleRepo {
 
 }
