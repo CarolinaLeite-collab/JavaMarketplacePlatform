391239b9b8ddefb8cb913b606ac4c9c3e362cd09
Carolina Leite <1251987@isep.ipp.pt>
2026-06-18 22:22:45 +0100

    Make ISaleRepo extend IRepository<SaleId, Sale> #1059

--- src/main/java/MITELOVERS/domain/repository/ISaleRepo.java
+++ src/main/java/MITELOVERS/domain/repository/ISaleRepo.java
@@ -1,11 +1,17 @@
 package MITELOVERS.domain.repository;
 
+import MITELOVERS.ddd.IRepository;
 import MITELOVERS.domain.sale.Sale;
+import MITELOVERS.domain.valueobject.SaleId;
 
 /**
  * Repository interface for managing persistence and retrieval of {@link Sale} aggregates.
+ * <p>
+ * Extends {@link IRepository} with {@link SaleId} as the identity type
+ * and {@link Sale} as the aggregate root type.
+ * </p>
  */
 
-public interface ISaleRepo {
+public interface ISaleRepo extends IRepository<SaleId, Sale> {
 
 }
