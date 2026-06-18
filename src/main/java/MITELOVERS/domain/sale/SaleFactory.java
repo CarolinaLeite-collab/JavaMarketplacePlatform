5aef4e7335701583d01ac922cd472e3e399495ab
Carolina Leite <1251987@isep.ipp.pt>
2026-06-18 16:32:59 +0100

    Create SaleFactory and tests #1065

--- src/main/java/MITELOVERS/domain/sale/SaleFactory.java
+++ src/main/java/MITELOVERS/domain/sale/SaleFactory.java
@@ -1,7 +1,12 @@
 package MITELOVERS.domain.sale;
 
-import MITELOVERS.domain.shoppingcart.ShoppingCart;
+import MITELOVERS.domain.valueobject.SaleId;
+import MITELOVERS.domain.valueobject.SaleSaleStatus;
+import MITELOVERS.domain.valueobject.UserId;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * Factory responsible for creating {@link Sale} instances.
@@ -10,4 +15,22 @@ import org.springframework.stereotype.Component;
 @Component
 public class SaleFactory {
 
+    public Sale createSale(UserId buyerId,
+                           List<SaleLine> saleLines) {
+
+        return new Sale(buyerId, saleLines);
+    }
+
+    public Sale reconstituteSale(SaleId saleId,
+                                 UserId buyerId,
+                                 List<SaleLine> saleLines,
+                                 LocalDateTime createdAt,
+                                 LocalDateTime completedAt,
+                                 SaleSaleStatus saleSaleStatus) {
+
+        return new Sale(saleId, buyerId, saleLines, createdAt, completedAt, saleSaleStatus
+        );
+    }
 }
+
+
