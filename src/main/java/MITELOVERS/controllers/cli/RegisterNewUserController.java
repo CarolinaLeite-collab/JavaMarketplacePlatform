1106970743c1902db1a80789ef86ac4d9f154fb9
Carolina Leite <1251987@isep.ipp.pt>
2026-06-21 11:45:05 +0100

    feat: create shopping cart on user registration Related to #1087

--- src/main/java/MITELOVERS/controllers/cli/RegisterNewUserController.java
+++ src/main/java/MITELOVERS/controllers/cli/RegisterNewUserController.java
@@ -8,6 +8,10 @@ import MITELOVERS.domain.valueobject.Email;
 import MITELOVERS.domain.valueobject.Name;
 import MITELOVERS.domain.valueobject.Phone;
 import org.springframework.stereotype.Controller;
+import MITELOVERS.domain.repository.IShoppingCartRepo;
+import MITELOVERS.domain.shoppingcart.ShoppingCart;
+import MITELOVERS.domain.shoppingcart.ShoppingCartFactory;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Controller responsible for handling the registration of a {@link User}.
@@ -19,12 +23,17 @@ public class RegisterNewUserController {
 
     private final IUserRepo _iUserRepo;
     private final UserFactory _userFactory;
+    private final IShoppingCartRepo _iShoppingCartRepo;
+    private final ShoppingCartFactory _shoppingCartFactory;
 
-    public RegisterNewUserController(IUserRepo userRepo, UserFactory userFactory) {
+    public RegisterNewUserController(IUserRepo userRepo, UserFactory userFactory, IShoppingCartRepo iShoppingCartRepo, ShoppingCartFactory shoppingCartFactory) {
         _iUserRepo = userRepo;
         _userFactory = userFactory;
+        _iShoppingCartRepo = iShoppingCartRepo;
+        _shoppingCartFactory = shoppingCartFactory;
     }
 
+    @Transactional
     public User registerNewUser(Name name, Address address, Email email, Phone phone) {
 
         User newUser = _userFactory.createUser(name, address, email, phone);
@@ -33,7 +42,13 @@ public class RegisterNewUserController {
             throw new IllegalStateException("User already exists");
         }
 
-        return _iUserRepo.save(newUser);
-    }
+        User createdUser = _iUserRepo.save(newUser);
+
+        ShoppingCart cart =
+                _shoppingCartFactory.createShoppingCart(createdUser.identity());
+
+        _iShoppingCartRepo.save(cart);
+
+        return createdUser;    }
 
 }
